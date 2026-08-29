3abb99c11c3a29021f2f31f44e37d752d290466c
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 19:35:39 -0400

    Open-BFME5: convert ATL CImage GetDC at 0x000015C8

--- Code/Libraries/Source/ATL/atlimage.cpp
+++ Code/Libraries/Source/ATL/atlimage.cpp
@@ -7,11 +7,13 @@ typedef HBITMAP__ *HBITMAP;
 struct HDC__;
 typedef HDC__ *HDC;
 
+typedef void *HGDIOBJ;
 typedef long LONG;
 
 extern "C" __declspec(dllimport) LONG __stdcall InterlockedExchange(volatile LONG *target, LONG value);
 extern "C" __declspec(dllimport) HDC __stdcall CreateCompatibleDC(HDC dc);
 extern "C" __declspec(dllimport) int __stdcall DeleteDC(HDC dc);
+extern "C" __declspec(dllimport) HGDIOBJ __stdcall SelectObject(HDC dc, HGDIOBJ object);
 
 namespace ATL
 {
@@ -33,6 +35,7 @@ public:
     virtual ~CImage() throw();
 
     HBITMAP Detach() throw();
+    HDC GetDC() const throw();
 
 private:
     HBITMAP m_hBitmap;
@@ -47,6 +50,8 @@ private:
     mutable HDC m_hDC;
     mutable int m_nDCRefCount;
     mutable HBITMAP m_hOldBitmap;
+
+    static CDCCache s_cache;
 };
 
 HDC CImage::CDCCache::GetDC() throw()
@@ -79,6 +84,18 @@ void CImage::CDCCache::ReleaseDC(HDC dc) throw()
     }
 
     DeleteDC(dc);
+}
+
+HDC CImage::GetDC() const throw()
+{
+    ++m_nDCRefCount;
+    if (m_hDC == 0)
+    {
+        m_hDC = s_cache.GetDC();
+        m_hOldBitmap = (HBITMAP)SelectObject(m_hDC, m_hBitmap);
+    }
+
+    return m_hDC;
 }
 
 HBITMAP CImage::Detach() throw()
