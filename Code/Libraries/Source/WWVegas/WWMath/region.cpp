ec96463fd29a62776b9fed5b13f7a9377f3d7343
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 14:07:05 -0400

    Open-BFME5: convert IRegion3D width at 0x00004E93
    Replace the gen_asm claim with the canonical x_max - x_min body. VC7.1 emits all 6 BFME2 retail bytes exactly; pinned BFME1 source, six-int layout, and retail bytes independently agree.

--- Code/Libraries/Source/WWVegas/WWMath/region.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/region.cpp
@@ -11,6 +11,20 @@ struct IRegion2D
     int y_max;
 };
 
+struct IRegion3D
+{
+    int width() const;
+    int height() const;
+    int depth() const;
+
+    int x_min;
+    int y_min;
+    int z_min;
+    int x_max;
+    int y_max;
+    int z_max;
+};
+
 int IRegion2D::width() const
 {
     return x_max - x_min;
@@ -19,4 +33,9 @@ int IRegion2D::width() const
 int IRegion2D::height() const
 {
     return y_max - y_min;
+}
+
+int IRegion3D::width() const
+{
+    return x_max - x_min;
 }
