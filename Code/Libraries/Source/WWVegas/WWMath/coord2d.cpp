5183b517dd53f18b90cce100970705679019a990
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 17:50:45 -0400

    Open-BFME5: convert Coord2D SetXAxis at 0x00003A4E

--- Code/Libraries/Source/WWVegas/WWMath/coord2d.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/coord2d.cpp
@@ -6,6 +6,7 @@ public:
     Coord2D(int x, int y);
     bool IsExactlyEqualTo(const Coord2D &that) const;
     Coord2D &Negate();
+    Coord2D &SetXAxis();
     Coord2D &SetZero();
 
     float x;
@@ -27,6 +28,13 @@ Coord2D &Coord2D::Negate()
 {
     x = -x;
     y = -y;
+    return *this;
+}
+
+Coord2D &Coord2D::SetXAxis()
+{
+    x = 1.0f;
+    y = 0.0f;
     return *this;
 }
 
