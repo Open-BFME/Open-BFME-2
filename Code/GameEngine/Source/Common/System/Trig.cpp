fb72850d6a6f7b99c95970ca8399b8059fb88495
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 15:20:48 -0400

    Open-BFME5: convert deg2rad at 0x0000352F

--- Code/GameEngine/Source/Common/System/Trig.cpp
+++ Code/GameEngine/Source/Common/System/Trig.cpp
@@ -1,5 +1,10 @@
 // cl: /ICode/Libraries/Include/Lib
 
+float deg2rad(float degrees)
+{
+    return degrees * 0.01745329238474369f;
+}
+
 float rad2deg(float radians)
 {
     return radians * 57.2957763671875f;
