f1f77c84b0c0d1e2bcde7fd51e1fff5d1ff15fec
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 19:24:54 -0400

    Open-BFME5: convert ATL CImage Detach at 0x000015A6

---
+++ Code/Libraries/Source/ATL/atlimage.cpp
@@ -0,0 +1,50 @@
+// cl: /O1
+// Microsoft Visual C++ .NET 2003 ATL 7.1 atlimage.h members.
+
+struct HBITMAP__;
+typedef HBITMAP__ *HBITMAP;
+
+struct HDC__;
+typedef HDC__ *HDC;
+
+namespace ATL
+{
+
+class CImage
+{
+public:
+    virtual ~CImage() throw();
+
+    HBITMAP Detach() throw();
+
+private:
+    HBITMAP m_hBitmap;
+    void *m_pBits;
+    int m_nWidth;
+    int m_nHeight;
+    int m_nPitch;
+    int m_nBPP;
+    bool m_bIsDIBSection;
+    bool m_bHasAlphaChannel;
+    long m_iTransparentColor;
+    mutable HDC m_hDC;
+    mutable int m_nDCRefCount;
+    mutable HBITMAP m_hOldBitmap;
+};
+
+HBITMAP CImage::Detach() throw()
+{
+    HBITMAP hBitmap = m_hBitmap;
+    m_hBitmap = 0;
+    m_pBits = 0;
+    m_nWidth = 0;
+    m_nHeight = 0;
+    m_nBPP = 0;
+    m_nPitch = 0;
+    m_iTransparentColor = -1;
+    m_bHasAlphaChannel = false;
+    m_bIsDIBSection = false;
+    return hBitmap;
+}
+
+}
