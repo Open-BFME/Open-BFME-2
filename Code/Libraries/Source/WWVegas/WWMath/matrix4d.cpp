a2a4dad51aa882f9a2e62e8001fa06c0321a9e2b
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 15:28:05 -0400

    Open-BFME5: convert Matrix4D Z vector accessor at 0x00004BEE

--- Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
@@ -13,6 +13,7 @@ class Matrix4D
 public:
     Coord3D &GetXVector(Coord3D &out) const;
     Coord3D &GetYVector(Coord3D &out) const;
+    Coord3D &GetZVector(Coord3D &out) const;
 
     float values[16];
 };
@@ -30,5 +31,13 @@ Coord3D &Matrix4D::GetYVector(Coord3D &out) const
     out.x = values[1];
     out.y = values[5];
     out.z = values[9];
+    return out;
+}
+
+Coord3D &Matrix4D::GetZVector(Coord3D &out) const
+{
+    out.x = values[2];
+    out.y = values[6];
+    out.z = values[10];
     return out;
 }
