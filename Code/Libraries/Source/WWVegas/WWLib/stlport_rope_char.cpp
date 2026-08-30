21f1e6eb958962ba5e9f95d75273cd3c4f9a297b
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 17:41:42 +0200

    Open-BFME5 Convert rope and valarray members

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_rope_char.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <rope>
+
+template class _STL::rope<char, _STL::allocator<char> >;
---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_valarray_int.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <valarray>
+
+template class _STL::valarray<int>;
