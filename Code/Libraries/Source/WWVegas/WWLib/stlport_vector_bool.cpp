1dbd55095ed49432a98001577e4686efcfb3b45a
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 17:34:23 +0200

    Open-BFME5 Convert vector bool and pair members

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_vector_bool.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <vector>
+
+template class _STL::vector<bool, _STL::allocator<bool> >;
---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_vector_pair.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <vector>
+
+template class _STL::vector<_STL::pair<int, int>, _STL::allocator<_STL::pair<int, int> > >;
