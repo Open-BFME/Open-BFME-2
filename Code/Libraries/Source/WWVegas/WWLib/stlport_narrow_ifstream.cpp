038cdbd73f07aa25cfc05cc136959947103cc68c
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 17:28:37 +0200

    Open-BFME5 Convert filebuf and fstream members

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_ifstream.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <fstream>
+
+template class _STL::basic_ifstream<char, _STL::char_traits<char> >;
---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_wide_stringbuf.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <sstream>
+
+template class _STL::basic_stringbuf<wchar_t, _STL::char_traits<wchar_t>, _STL::allocator<wchar_t> >;
