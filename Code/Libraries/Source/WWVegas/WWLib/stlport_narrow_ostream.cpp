caa51bfee530d0f8efe705c347bd2509333dcab7
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 17:23:56 +0200

    Open-BFME5 Convert wide streambuf and narrow ostream members

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_ostream.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <ostream>
+
+template class _STL::basic_ostream<char, _STL::char_traits<char> >;
---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_wide_streambuf.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <streambuf>
+
+template class _STL::basic_streambuf<wchar_t, _STL::char_traits<wchar_t> >;
