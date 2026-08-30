e398e81251c13c46964bdb31104e98e315c3c002
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 21:06:54 -0400

    Open-BFME5: convert Matrix4D addition assignment at 0x00004309

--- Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
@@ -18,6 +18,7 @@ public:
         float m20, float m21, float m22, float m23,
         float m30, float m31, float m32, float m33);
     Matrix4D &operator*=(float scale);
+    Matrix4D &operator+=(const Matrix4D &that);
     Coord3D &GetXVector(Coord3D &out) const;
     Coord3D &GetYVector(Coord3D &out) const;
     Coord3D &GetZVector(Coord3D &out) const;
@@ -93,6 +94,27 @@ Matrix4D &Matrix4D::operator*=(float scale)
     values[13] *= scale;
     values[14] *= scale;
     values[15] *= scale;
+    return *this;
+}
+
+Matrix4D &Matrix4D::operator+=(const Matrix4D &that)
+{
+    values[0] += that.values[0];
+    values[1] += that.values[1];
+    values[2] += that.values[2];
+    values[3] += that.values[3];
+    values[4] += that.values[4];
+    values[5] += that.values[5];
+    values[6] += that.values[6];
+    values[7] += that.values[7];
+    values[8] += that.values[8];
+    values[9] += that.values[9];
+    values[10] += that.values[10];
+    values[11] += that.values[11];
+    values[12] += that.values[12];
+    values[13] += that.values[13];
+    values[14] += that.values[14];
+    values[15] += that.values[15];
     return *this;
 }
 
