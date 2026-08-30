8716f9b431e2b62cf9fd7053ba3a0349c594ce7d
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 21:03:59 -0400

    Open-BFME5: convert Matrix4D 16-float constructor at 0x00005C2C

--- Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
@@ -12,6 +12,11 @@ class Matrix4D
 {
 public:
     Matrix4D(const Matrix4D &that);
+    Matrix4D(
+        float m00, float m01, float m02, float m03,
+        float m10, float m11, float m12, float m13,
+        float m20, float m21, float m22, float m23,
+        float m30, float m31, float m32, float m33);
     Coord3D &GetXVector(Coord3D &out) const;
     Coord3D &GetYVector(Coord3D &out) const;
     Coord3D &GetZVector(Coord3D &out) const;
@@ -43,6 +48,30 @@ Matrix4D::Matrix4D(const Matrix4D &that)
         values[14] = that.values[14];
         values[15] = that.values[15];
     }
+}
+
+Matrix4D::Matrix4D(
+    float m00, float m01, float m02, float m03,
+    float m10, float m11, float m12, float m13,
+    float m20, float m21, float m22, float m23,
+    float m30, float m31, float m32, float m33)
+{
+    values[0] = m00;
+    values[1] = m01;
+    values[2] = m02;
+    values[3] = m03;
+    values[4] = m10;
+    values[5] = m11;
+    values[6] = m12;
+    values[7] = m13;
+    values[8] = m20;
+    values[9] = m21;
+    values[10] = m22;
+    values[11] = m23;
+    values[12] = m30;
+    values[13] = m31;
+    values[14] = m32;
+    values[15] = m33;
 }
 
 Coord3D &Matrix4D::GetXVector(Coord3D &out) const
