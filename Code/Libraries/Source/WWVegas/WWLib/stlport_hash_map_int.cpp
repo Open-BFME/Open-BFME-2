7985297ded7b8c5bece700e7ab9efbe5634322e8
302596555 <302596555@student.rocmondriaan.nl>
2026-08-30 17:27:21 +0200

    Open-BFME5 Convert associative container members

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_hash_map_int.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <hash_map>
+
+template class _STL::hash_map<int, int, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, int> > >;
---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_map_int_int.cpp
@@ -0,0 +1,6 @@
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <map>
+
+template class _STL::map<int, int, _STL::less<int>, _STL::allocator<_STL::pair<const int, int> > >;
