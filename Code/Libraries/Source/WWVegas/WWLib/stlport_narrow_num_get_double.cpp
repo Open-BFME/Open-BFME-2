ac41d1acef11d2e62ac93670763364a1b59840a7
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 22:57:55 -0400

    Open-BFME5: convert long double formatter at 0x0000FE80

--- Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
@@ -29,7 +29,15 @@ template <class CharT, class Traits, class Alloc>
 class basic_string
 {
 public:
+	typedef unsigned int size_type;
+
 	basic_string();
+	void reserve(size_type amount);
+	__declspec(dllimport) __forceinline const CharT *c_str() const { return _M_start; }
+	__declspec(dllimport) __forceinline size_type size() const
+	{
+		return static_cast<size_type>(_M_finish - _M_start);
+	}
 	__forceinline ~basic_string()
 	{
 		if (_M_start != 0)
@@ -44,16 +52,55 @@ private:
 
 template basic_string<char, char_traits<char>, allocator<char> >::~basic_string();
 
+typedef basic_string<char, char_traits<char>, allocator<char> > narrow_string;
+
 class ios_base
 {
 public:
+	typedef int fmtflags;
 	typedef int iostate;
+	typedef int openmode;
+	typedef int seekdir;
+	typedef int streamsize;
+
 	enum
 	{
 		goodbit = 0,
 		eofbit = 2,
 		failbit = 4
 	};
+
+	virtual ~ios_base();
+	__declspec(dllimport) __forceinline fmtflags flags() const { return _M_fmtflags; }
+	__declspec(dllimport) __forceinline streamsize precision() const { return _M_precision; }
+	__declspec(dllimport) __forceinline const void *_M_numpunct_facet() const
+	{
+		return _M_cached_numpunct;
+	}
+	__declspec(dllimport) __forceinline const narrow_string &_M_grouping() const
+	{
+		return _M_cached_grouping;
+	}
+
+private:
+	fmtflags _M_fmtflags;
+	iostate _M_iostate;
+	openmode _M_openmode;
+	seekdir _M_seekdir;
+	iostate _M_exception_mask;
+	streamsize _M_precision;
+	streamsize _M_width;
+	void *_M_locale;
+	void *_M_callbacks;
+	unsigned int _M_num_callbacks;
+	unsigned int _M_callback_index;
+	long *_M_iwords;
+	unsigned int _M_num_iwords;
+	void **_M_pwords;
+	unsigned int _M_num_pwords;
+	void *_M_cached_ctype;
+	void *_M_cached_numpunct;
+	narrow_string _M_cached_grouping;
 };
 
 template <class CharT, class Traits>
@@ -69,7 +116,6 @@ private:
 	unsigned char _M_have_c;
 };
 
-typedef basic_string<char, char_traits<char>, allocator<char> > narrow_string;
 typedef istreambuf_iterator<char, char_traits<char> > narrow_iterator;
 
 template <class InputIter, class CharT>
@@ -118,5 +164,64 @@ narrow_iterator num_get<char, narrow_iterator>::do_get(
 		error |= ios_base::eofbit;
 	return in;
 }
+
+// STLport 4.6 narrow numeric output helper. BFME2's retail object uses the
+// 4.6 string-backed formatter here rather than the vendored 4.5.3 fixed buffer.
+
+template <class CharT, class Traits>
+class ostreambuf_iterator
+{
+private:
+	void *_M_buf;
+	bool _M_ok;
+};
+
+template <class CharT>
+class numpunct;
+
+template <>
+class numpunct<char>
+{
+public:
+	__declspec(dllimport) __forceinline char decimal_point() const { return do_decimal_point(); }
+	__declspec(dllimport) __forceinline char thousands_sep() const { return do_thousands_sep(); }
+
+protected:
+	virtual ~numpunct();
+	virtual char do_decimal_point() const;
+	virtual char do_thousands_sep() const;
+};
+
+void __cdecl __write_float(
+		narrow_string &, ios_base::fmtflags, int, long double);
+
+template <class OutputIter>
+OutputIter __cdecl __put_float(
+		char *, char *, OutputIter, ios_base &, char, char, char,
+		const narrow_string &);
+
+template <class CharT, class OutputIter, class Float>
+OutputIter __cdecl _M_do_put_float(
+		OutputIter out, ios_base &stream, CharT fill, Float value)
+{
+	narrow_string buffer;
+	buffer.reserve(128);
+	__write_float(buffer, stream.flags(), static_cast<int>(stream.precision()), value);
+
+	const numpunct<CharT> &punctuation =
+			*static_cast<const numpunct<CharT> *>(stream._M_numpunct_facet());
+
+	return __put_float(
+			const_cast<char *>(buffer.c_str()),
+			const_cast<char *>(buffer.c_str()) + buffer.size(),
+			out, stream, fill, punctuation.decimal_point(),
+			punctuation.thousands_sep(), stream._M_grouping());
+}
+
+typedef ostreambuf_iterator<char, char_traits<char> > narrow_output_iterator;
+
+template narrow_output_iterator _M_do_put_float<
+		char, narrow_output_iterator, long double>(
+		narrow_output_iterator, ios_base &, char, long double);
 
 }
