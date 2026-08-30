ccb4cb8aaaee0d6787261a22513fbfdaa547a21a
Jonathan <jonathan@users.noreply.github.com>
2026-08-29 22:38:22 -0400

    Open-BFME5: convert narrow num_get double at 0x0000FCC0

--- Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_narrow_num_get_double.cpp
@@ -1,9 +1,11 @@
-// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// cl: /EHs /EHc- /MD /D_STLP_USE_STATIC_LIB
 // stlport
 //
 // STLport 4.5.3 narrow basic_string destructor emitted by the num_get<double>
 // instantiation. The retail unwind map for 0x0000FCC0 selects the copy at
 // 0x00142D70.
+// /EHc must remain disabled so VC7.1 retains the retail unwind-state update
+// around the C-linkage free call.
 
 namespace _STL
 {
@@ -27,6 +29,7 @@ template <class CharT, class Traits, class Alloc>
 class basic_string
 {
 public:
+	basic_string();
 	__forceinline ~basic_string()
 	{
 		if (_M_start != 0)
@@ -40,5 +43,62 @@ private:
 };
 
 template basic_string<char, char_traits<char>, allocator<char> >::~basic_string();
+
+class ios_base
+{
+public:
+	typedef int iostate;
+	enum
+	{
+		goodbit = 0,
+		eofbit = 2,
+		failbit = 4
+	};
+};
+
+template <class CharT, class Traits>
+class istreambuf_iterator
+{
+public:
+	bool equal(const istreambuf_iterator &) const;
+
+private:
+	void *_M_buf;
+	CharT _M_c;
+	unsigned char _M_eof;
+	unsigned char _M_have_c;
+};
+
+typedef basic_string<char, char_traits<char>, allocator<char> > narrow_string;
+typedef istreambuf_iterator<char, char_traits<char> > narrow_iterator;
+
+template <class InputIter, class CharT>
+bool __cdecl _M_read_float(
+		narrow_string &, InputIter &, InputIter &, ios_base &, CharT *);
+
+void __cdecl __string_to_float(const narrow_string &, double &);
+
+template <class CharT, class InputIter>
+class num_get
+{
+protected:
+	virtual InputIter do_get(
+			InputIter, InputIter, ios_base &, ios_base::iostate &, double &) const;
+};
+
+template <>
+narrow_iterator num_get<char, narrow_iterator>::do_get(
+		narrow_iterator in, narrow_iterator end, ios_base &stream,
+		ios_base::iostate &error, double &value) const
+{
+	narrow_string buffer;
+	bool ok = _M_read_float(buffer, in, end, stream, (char *)0);
+	__string_to_float(buffer, value);
+	error = static_cast<ios_base::iostate>(
+			ok ? ios_base::goodbit : ios_base::failbit);
+	if (in.equal(end))
+		error |= ios_base::eofbit;
+	return in;
+}
 
 }
