5b32cef584b53ff1343d87364b964ade98bed072
Jonathan <jonathan@users.noreply.github.com>
2026-09-05 01:04:28 -0400

    Bank money_get string conversion partial

---
+++ reverse/attempts/0x00011230.cpp
@@ -0,0 +1,9 @@
+// ?do_get@?$money_get@DV?$istreambuf_iterator@DV?$char_traits@D@_STL@@@_STL@@@_STL@@MBE?AV?$istreambuf_iterator@DV?$char_traits@D@_STL@@@2@V32@0_NAAVios_base@2@AAHAAV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@2@@Z
+// partial score=0.094937 date=2026-09-05
+#pragma inline_depth(1)
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <locale>
+
+template class _STL::money_get<char, _STL::istreambuf_iterator<char, _STL::char_traits<char> > >;
