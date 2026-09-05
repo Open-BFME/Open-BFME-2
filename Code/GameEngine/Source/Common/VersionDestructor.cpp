8a771b681ad7a6fad597f2b4c09363aba95b10fd
Jonathan <jonathan@users.noreply.github.com>
2026-09-04 23:58:06 -0400

    Recover Version destructor and seven retail string members

---
+++ Code/GameEngine/Source/Common/VersionDestructor.cpp
@@ -0,0 +1,51 @@
+// cl: /O1 /EHsc
+// Version lifetime recovered from BFME2 retail and Open-BFME-1 version.cpp.
+// WinMain deletes TheVersion (VA 0xDFE770) through this destructor.
+// Constructor 0x238852 initializes seven strings; initializer 0x23870E
+// identifies their fields by ID, MACHINE, USER, GUID, TIME, DATE and CONFIG.
+template <typename T>
+class StringBase
+{
+	friend class AsciiString;
+
+private:
+	struct Header
+	{
+		int ref_count;
+		unsigned short length;
+		unsigned short capacity;
+		T data[1];
+	};
+
+	void releaseBuffer();
+	Header *m_data;
+};
+
+class AsciiString
+{
+public:
+	~AsciiString()
+	{
+		m_data.releaseBuffer();
+	}
+
+private:
+	StringBase<char> m_data;
+};
+
+class Version
+{
+public:
+    ~Version();
+private:
+    int m_major, m_minor, m_buildNum, m_localBuildNum;
+    AsciiString m_buildTitle;
+    AsciiString m_buildLocation;
+    AsciiString m_buildUser;
+    AsciiString m_buildGuid;
+    AsciiString m_buildTime;
+    AsciiString m_buildDate;
+    AsciiString m_buildConfiguration;
+    bool m_showFullVersion;
+};
+Version::~Version() {}
