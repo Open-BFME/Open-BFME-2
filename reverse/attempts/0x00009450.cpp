2756bcdf34a4c9c4dff3dea460fc69469f8ad3bb
Jonathan <jonathan@users.noreply.github.com>
2026-09-04 22:38:11 -0400

    Record remaining dump-family blockers and bank num_put attempt

---
+++ reverse/attempts/0x00009450.cpp
@@ -0,0 +1,8 @@
+// ?do_put@?$num_put@DV?$ostreambuf_iterator@DV?$char_traits@D@_STL@@@_STL@@@_STL@@MBE?AV?$ostreambuf_iterator@DV?$char_traits@D@_STL@@@2@V32@AAVios_base@2@D_N@Z
+// partial score=0.8 date=2026-09-04
+// cl: /EHsc /MD /D_STLP_USE_STATIC_LIB /D_STLP_USE_MALLOC
+// stlport
+
+#include <locale>
+
+template class _STL::num_put<char, _STL::ostreambuf_iterator<char, _STL::char_traits<char> > >;
