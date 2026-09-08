bfe35023b7c9ac3595cc650cbbbd475372f9db75
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-07 20:31:46 -0400

    Reconstruct RenderObj assignment and its destination-factor loop (217 bytes)

---
+++ Code/Libraries/Source/WWVegas/WW3D2/RenderObjAssign.cpp
@@ -0,0 +1,41 @@
+// cl: /Ireference/shims/bfme2renderobj /arch:SSE /G7 /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "rendobj.h"
+#include "aabox.h"
+
+typedef char RenderObjSizeMatchesRetail[(sizeof(RenderObjClass) == 0xC4) ? 1 : -1];
+
+// The retail assignment copies transform storage and its identity byte after
+// the visibility/collision virtuals. Both indexed-factor calls use this object
+// as the receiver; the loop is not a copy from that.
+RenderObjClass &RenderObjClass::operator=(const RenderObjClass &that)
+{
+    if (this != &that) {
+        Set_Hidden(that.Is_Hidden());
+        Set_Animation_Hidden(that.Is_Animation_Hidden());
+        Set_Force_Visible(that.Is_Force_Visible());
+        Set_Collision_Type(that.Get_Collision_Type());
+        Transform = that.Transform;
+        IsTransformIdentity = that.IsTransformIdentity;
+        for (int i = 0; i < 2; ++i) {
+            _bfme_set_indexed_factor(i, _bfme_get_indexed_factor(i));
+        }
+    }
+    return *this;
+}
