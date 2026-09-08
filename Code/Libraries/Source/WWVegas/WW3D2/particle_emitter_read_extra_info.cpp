198a89c25c78357bfe240b89445da26c88227497
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-08 06:05:41 -0400

    Recover particle emitter extra info reader

---
+++ Code/Libraries/Source/WWVegas/WW3D2/particle_emitter_read_extra_info.cpp
@@ -0,0 +1,54 @@
+// cl: /Ireference/shims/bfmeparticleload /EHsc /Ireference/shims /Ireference/shims/bfmerendobj /arch:SSE /G7 /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+/*
+** Command & Conquer Generals Zero Hour(tm)
+** Copyright 2025 Electronic Arts Inc.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sweep/winbase_shim.h>
+#include "rendobj.h"	// the bfmerendobj shim has to win the include guard
+#include "winbase_shim.h"
+#define MAX_PATH 260
+#define LPCTSTR const char *
+#include "part_ldr.h"
+#include "part_emt.h"
+#include "w3derr.h"
+#include "chunkio.h"
+#include "win.h"
+#include "assetmgr.h"
+#include "texture.h"
+
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(pointer) \
+{ \
+	if (pointer) { \
+		delete pointer; \
+		pointer = 0; \
+	} \
+}
+#endif
+
+// Particle system loader, adapted from the Open-BFME-1 donor for BFME2's bool loader ABI.
+bool
+ParticleEmitterDefClass::Read_Extra_Info (ChunkLoadClass &chunk_load)
+{
+	bool ret_val = false;
+
+	::memset (&m_ExtraInfo, 0, sizeof (m_ExtraInfo));
+	if (chunk_load.Read (&m_ExtraInfo, sizeof (m_ExtraInfo)) == sizeof (m_ExtraInfo)) {
+		ret_val = true;
+	}
+	return ret_val;
+}
