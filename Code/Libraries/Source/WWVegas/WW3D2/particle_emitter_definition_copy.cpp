468d8b06177fb66936222ed65310b9d78d0c593e
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-08 06:06:49 -0400

    Recover particle emitter definition copy

---
+++ Code/Libraries/Source/WWVegas/WW3D2/particle_emitter_definition_copy.cpp
@@ -0,0 +1,79 @@
+// cl: /Ireference/shims/bfmeshader /Ireference/shims/bfmeparticleload /EHsc /Ireference/shims/bfmeparticlehandle /Ireference/shims /Ireference/shims/bfmerendobj /arch:SSE /G7 /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+// Select the BFME2 legacy kernel32 import declarations before reference headers.
+#define _CRTIMP
+#include <sweep/winbase_shim.h>
+
+#include "rendobj.h"	// the bfmerendobj shim has to win the include guard
+#include "winbase_shim.h"
+#define MAX_PATH 260
+#define LPCTSTR const char *
+// Particle system loader, adapted from the Generals Zero Hour reference.
+/***********************************************************************************************
+ ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
+ ***********************************************************************************************
+ *                                                                                             *
+ *                 Project Name : WW3D                                                         *
+ *                                                                                             *
+ *                     $Archive:: /VSS_Sync/ww3d2/part_ldr.cpp              $*
+ *                                                                                             *
+ *                       Author:: Patrick Smith                                                
+ *                                                                                             *
+ *                     $Modtime:: 10/26/01 2:57p                                              $*
+ *                                                                                             *
+ *                    $Revision:: 11                                                          $*
+ *                                                                                             *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+
+#include "shader.h"
+#include "part_ldr.h"
+#include "part_emt.h"
+#include "w3derr.h"
+#include "chunkio.h"
+#include "assetmgr.h"
+#include "texture.h"
+
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(pointer) \
+{ \
+	if (pointer) {	\
+		::delete pointer; \
+		pointer = 0; \
+	} \
+}
+#endif //SAFE_DELETE
+
+#ifndef SAFE_DELETE_ARRAY
+#define SAFE_DELETE_ARRAY(pointer)	\
+	if (pointer) {					\
+		delete [] pointer;			\
+		pointer = 0;				\
+	}									\
+
+#endif //SAFE_DELETE
+
+
+ParticleEmitterDefClass::ParticleEmitterDefClass (const ParticleEmitterDefClass &src)
+	:	m_pName (NULL),
+		m_Version (0L),
+		m_pUserString (NULL),	  
+		m_iUserType (EMITTER_TYPEID_DEFAULT),
+		m_InitialOrientationRandom (src.m_InitialOrientationRandom),
+		m_pCreationVolume (NULL),
+		m_pVelocityRandomizer (NULL)
+{
+	::memset (&m_Info, 0, sizeof (m_Info));
+	::memset (&m_InfoV2, 0, sizeof (m_InfoV2));	
+	::memset (&m_ExtraInfo, 0, sizeof (m_ExtraInfo));	
+
+	::memset (&m_ColorKeyframes, 0, sizeof (m_ColorKeyframes));
+	::memset (&m_OpacityKeyframes, 0, sizeof (m_OpacityKeyframes));
+	::memset (&m_SizeKeyframes, 0, sizeof (m_SizeKeyframes));
+	::memset (&m_RotationKeyframes, 0, sizeof (m_RotationKeyframes));
+	::memset (&m_FrameKeyframes, 0, sizeof (m_FrameKeyframes));
+	::memset (&m_BlurTimeKeyframes, 0, sizeof (m_BlurTimeKeyframes));
+	::memset (&m_LineProperties, 0, sizeof (m_LineProperties));
+
+	(*this) = src;
+	return ;
+}
