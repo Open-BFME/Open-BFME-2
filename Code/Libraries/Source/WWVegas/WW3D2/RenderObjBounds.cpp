2773fd23f055f508b6d5e4899aa0e94bc2693486
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-07 20:03:56 -0400

    Reconstruct RenderObj cached bounding-volume update with the retail layout (305 bytes)
    A dedicated TU and scoped 0xC4 RenderObj header place the cached Sphere at0x4C and AABox at0x5C, retaining the sphere scale at0x48. The complete305-byte cache-update routine now matches every instruction, its four virtual calls and its named Matrix3D helper. Unknown fields retain neutral names and no complete-vtable claim is made. No shared header or consistency baseline changes.

---
+++ Code/Libraries/Source/WWVegas/WW3D2/RenderObjBounds.cpp
@@ -0,0 +1,39 @@
+// cl: /Ireference/shims/bfme2renderobj /arch:SSE /G7 /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "rendobj.h"
+#include "aabox.h"
+
+typedef char RenderObjSizeMatchesRetail[(sizeof(RenderObjClass) == 0xC4) ? 1 : -1];
+
+void RenderObjClass::Update_Cached_Bounding_Volumes(void) const
+{
+	Get_Obj_Space_Bounding_Box(CachedBoundingBox);
+	Get_Obj_Space_Bounding_Sphere(CachedBoundingSphere);
+
+#ifdef ALLOW_TEMPORARIES
+	CachedBoundingSphere.Center = Get_Transform() * CachedBoundingSphere.Center;
+#else
+	Get_Transform().mulVector3(CachedBoundingSphere.Center);
+#endif
+ 	CachedBoundingSphere.Radius = ObjectScale * CachedBoundingSphere.Radius;
+	CachedBoundingBox.Transform(Get_Transform());
+
+	Validate_Cached_Bounding_Volumes();
+}
+
