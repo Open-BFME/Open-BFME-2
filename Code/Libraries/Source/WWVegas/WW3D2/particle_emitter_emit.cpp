32d7f807fb685908155bddeaef869b3ac6a291bc
jonathan-farmer1 <142048329+jonathan-farmer1@users.noreply.github.com>
2026-09-08 05:32:42 -0400

    Recover particle emitter emission with origin snapshot

---
+++ Code/Libraries/Source/WWVegas/WW3D2/particle_emitter_emit.cpp
@@ -0,0 +1,81 @@
+// cl: /G7 /Ireference/shims/bfmerendobj /arch:SSE /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*************************************************************************** 
+ ***    C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S     *** 
+ *************************************************************************** 
+ *                                                                         * 
+ *                 Project Name : G                                        * 
+ *                                                                         * 
+ *                     $Archive:: /Commando/Code/ww3d2/part_emt.cpp       $* 
+ *                                                                         * 
+ *                  $Org Author:: Jani_p                                  $* 
+ *                                                                         * 
+ *                      $Author:: Kenny_m                                  $* 
+ *                                                                         * 
+ *                     $Modtime:: 08/05/02 10:44a                          $* 
+ *                                                                         * 
+ *                    $Revision:: 14                                      $* 
+ *                                                                         * 
+ * 08/05/02 KM Texture class redesign
+ *-------------------------------------------------------------------------* 
+ * Functions:                                                              * 
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+#include "rendobj.h"
+#include "part_emt.h"
+#include "wwdebug.h"
+#include "ww3d.h"
+#include "assetmgr.h"
+#include "part_ldr.h"
+#include "w3derr.h"
+#include "scene.h"
+#include "texture.h"
+#include "wwprofile.h"
+#include <limits.h>
+#include "gcd_lcm.h"
+
+void ParticleEmitterClass::Emit(void)
+{
+	WWPROFILE("PartlicleEmitter::Emit");
+#ifdef WWDEBUG
+	if (DebugDisable == true) {
+		return;
+	}
+#endif
+
+	if (Active && !IsComplete) {
+		Quaternion curr_quat;   // Quaternion form of orientation.
+		Vector3 curr_orig;      // Origin.
+	   
+	   // Convert current matrix into quaternion + origin form.
+	   curr_quat = Build_Quaternion(Get_Transform());
+	   curr_orig = Get_Transform().Get_Translation();
+
+	   // Keep the origin snapshot separate from the stored previous-origin value.
+	   Create_New_Particles(curr_quat, Vector3(curr_orig));
+
+	   PrevQ = curr_quat;
+	   PrevOrig = curr_orig;
+	} else {
+		// These need to be updated each frame no matter what
+	   PrevQ = Build_Quaternion(Get_Transform());
+	   PrevOrig = Get_Transform().Get_Translation();
+	}
+}
