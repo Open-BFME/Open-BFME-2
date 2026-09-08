3cb7ab5540f06738c2228a10a050ee7c7393f00c
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-08 14:28:52 -0700

    Open-BFME2: claim PowerVR device mapper

--- Code/Libraries/Source/WWVegas/WW3D2/dx8caps.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/dx8caps.cpp
@@ -441,7 +441,7 @@ DX8Caps::DeviceTypeMatrox DX8Caps::Get_Matrox_Device(unsigned device_id)
 	}
 }
  
-// ?Get_PowerVR_Device@DX8Caps@@CA?AW4DeviceTypePowerVR@1@I@Z present-unmatched
+// ?Get_PowerVR_Device@DX8Caps@@CA?AW4DeviceTypePowerVR@1@I@Z
 DX8Caps::DeviceTypePowerVR DX8Caps::Get_PowerVR_Device(unsigned device_id)
 {
 	switch (device_id) {
