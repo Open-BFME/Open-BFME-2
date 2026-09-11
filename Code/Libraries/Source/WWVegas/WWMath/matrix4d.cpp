70d89ae1cc922bf336f1430e22f98c366e002b69
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-11 12:21:12 -0700

    Port BFME1 Matrix4D 16-float setter to BFME2

--- Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/matrix4d.cpp
@@ -36,6 +36,11 @@ public:
     Matrix4D &SetIdentity();
     Matrix4D &Multiply(const Matrix4D &left, const Matrix4D &right);
     Matrix4D &Set(const Coord3D &translation);
+    Matrix4D &Set(
+        float m00, float m01, float m02, float m03,
+        float m10, float m11, float m12, float m13,
+        float m20, float m21, float m22, float m23,
+        float m30, float m31, float m32, float m33);
 
     bool IsExactlyEqualTo(const Matrix4D &that);
     Matrix4D &Transpose();
@@ -509,6 +514,33 @@ Matrix4D &Matrix4D::Set(const Coord3D &translation)
     values[3] = translation.x;
     values[7] = translation.y;
     values[11] = translation.z;
+
+    return *this;
+}
+
+// The same sixteen stores as the sixteen-float constructor, folded onto it.
+Matrix4D &Matrix4D::Set(
+    float m00, float m01, float m02, float m03,
+    float m10, float m11, float m12, float m13,
+    float m20, float m21, float m22, float m23,
+    float m30, float m31, float m32, float m33)
+{
+    values[0] = m00;
+    values[1] = m01;
+    values[2] = m02;
+    values[3] = m03;
+    values[4] = m10;
+    values[5] = m11;
+    values[6] = m12;
+    values[7] = m13;
+    values[8] = m20;
+    values[9] = m21;
+    values[10] = m22;
+    values[11] = m23;
+    values[12] = m30;
+    values[13] = m31;
+    values[14] = m32;
+    values[15] = m33;
 
     return *this;
 }
