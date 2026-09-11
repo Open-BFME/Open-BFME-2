8d0df052b90af98070356d5bed67aa95b4f4a3e7
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-11 14:57:10 -0700

    Port BFME1 lightning emission template destructor to BFME2

--- Code/GameEngine/Source/GameClient/FXParticleSystemDefaultTemplateDtor.cpp
+++ Code/GameEngine/Source/GameClient/FXParticleSystemDefaultTemplateDtor.cpp
@@ -168,4 +168,20 @@ LightningDrawModuleTemplate::~LightningDrawModuleTemplate()
     *(volatile unsigned int *)this = 0x00BBB52C;
 }
 
+class __declspec(novtable) LightningEmissionModuleTemplate
+{
+public:
+    virtual ~LightningEmissionModuleTemplate();
+};
+
+LightningEmissionModuleTemplate::~LightningEmissionModuleTemplate()
+{
+    unsigned char *info = this ? (unsigned char *)this + 8 : 0;
+    *(volatile unsigned int *)info = 0x00BBB554;
+
+    unsigned char *base = this ? (unsigned char *)this + 4 : 0;
+    *(volatile unsigned int *)base = 0x00C1C780;
+    *(volatile unsigned int *)this = 0x00BBB52C;
+}
+
 }
