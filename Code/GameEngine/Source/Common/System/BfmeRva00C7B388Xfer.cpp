63c83aa35838dc309b5354e89884a04dfce3d34f
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-11 15:40:08 +0200

    Land the text-dump Xfer unsigned char transfer at 0x60D825.

--- Code/GameEngine/Source/Common/System/BfmeRva00C7B388Xfer.cpp
+++ Code/GameEngine/Source/Common/System/BfmeRva00C7B388Xfer.cpp
@@ -250,7 +250,6 @@ Xfer &BfmeRva00C7B388::operator==(char &value)
     return *this;
 }
 
-// ??8BfmeRva00C7B388@@UAEAAVXfer@@AAE@Z present-unmatched
 Xfer &BfmeRva00C7B388::operator==(unsigned char &value)
 {
     if (!m_bfme04)
