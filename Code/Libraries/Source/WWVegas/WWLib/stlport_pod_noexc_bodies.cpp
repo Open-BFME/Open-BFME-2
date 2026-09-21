4fa22df9525c41d46c5c31c6dbadb8b498b7155d
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-21 22:24:24 +0200

    Recover 9 more _STLP_NO_EXCEPTIONS bodies incl. deque<BuddyRequest> map setup
    Large-size no-exceptions sweep: vector::_M_insert_overflow for
    156/260/872-byte elements (197B each) and _Deque_base::_M_initialize_map
    (131B) plus _M_create_nodes (36B) for 492/840-byte placeholders and for
    BuddyRequest, whose deque family was identified in d2038c0fa. Five
    callee pins, all at new addresses.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_pod_noexc_bodies.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_pod_noexc_bodies.cpp
@@ -9,6 +9,7 @@
 // compiled here. No retail caller is claimed.
 #include <vector>
 #include <deque>
+#include <hash_map>
 struct BfmePod8 { int a[2]; };
 struct BfmePod16 { int a[4]; };
 struct BfmePod20 { int a[5]; };
@@ -19,6 +20,20 @@ struct BfmePod44 { int a[11]; };
 struct BfmePod52 { int a[13]; };
 struct BfmePod68 { int a[17]; };
 struct BfmePod76 { int a[19]; };
+struct BfmePod156 { int a[39]; };
+struct BfmePod260 { int a[65]; };
+struct BfmePod872 { int a[218]; };
+struct BfmePod492 { int a[123]; };
+struct BfmePod840 { int a[210]; };
+struct BfmePod24 { int a[6]; };
+inline bool operator==(const BfmePod16 &x, const BfmePod16 &y) { return x.a[0] == y.a[0]; }
+inline bool operator==(const BfmePod24 &x, const BfmePod24 &y) { return x.a[0] == y.a[0]; }
+inline bool operator==(const BfmePod52 &x, const BfmePod52 &y) { return x.a[0] == y.a[0]; }
+// 696 = 0x2B8 is BuddyRequest, as in stlport_pod_large_bodies.cpp.
+class BuddyRequest
+{
+	char m_bfmeBody[0x2B8];
+};
 template class _STL::vector<BfmePod28, _STL::allocator<BfmePod28 > >;
 template class _STL::vector<BfmePod32, _STL::allocator<BfmePod32 > >;
 template class _STL::vector<BfmePod40, _STL::allocator<BfmePod40 > >;
@@ -30,3 +45,12 @@ template class _STL::deque<BfmePod8, _STL::allocator<BfmePod8 > >;
 template class _STL::deque<BfmePod16, _STL::allocator<BfmePod16 > >;
 template class _STL::deque<BfmePod20, _STL::allocator<BfmePod20 > >;
 template class _STL::deque<BfmePod28, _STL::allocator<BfmePod28 > >;
+template class _STL::vector<BfmePod156, _STL::allocator<BfmePod156 > >;
+template class _STL::vector<BfmePod260, _STL::allocator<BfmePod260 > >;
+template class _STL::vector<BfmePod872, _STL::allocator<BfmePod872 > >;
+template class _STL::deque<BfmePod492, _STL::allocator<BfmePod492 > >;
+template class _STL::deque<BuddyRequest, _STL::allocator<BuddyRequest > >;
+template class _STL::deque<BfmePod840, _STL::allocator<BfmePod840 > >;
+template class _STL::hash_map<int, BfmePod16, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod16> > >;
+template class _STL::hash_map<int, BfmePod24, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod24> > >;
+template class _STL::hash_map<int, BfmePod52, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod52> > >;
