7a9794a05fe0c1f49a955eb75717b4bff5e78b1c
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-21 21:46:24 +0200

    Recover 5 more hash_map placeholder bodies (44/52/60/72-byte values)
    hashtable::_M_new_node for 44/60/72-byte values and pair<const int, BfmePod52> value/copy ctors plus _Construct; three _Construct callee pins at new addresses. Two node-kind allocate hits are deny-listed under the rule of the previous commit.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_pod_hash_bodies.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_pod_hash_bodies.cpp
@@ -18,9 +18,21 @@ struct BfmePod32 { int a[8]; };
 inline bool operator==(const BfmePod32 &x, const BfmePod32 &y) { return x.a[0] == y.a[0]; }
 struct BfmePod48 { int a[12]; };
 inline bool operator==(const BfmePod48 &x, const BfmePod48 &y) { return x.a[0] == y.a[0]; }
+struct BfmePod44 { int a[11]; };
+inline bool operator==(const BfmePod44 &x, const BfmePod44 &y) { return x.a[0] == y.a[0]; }
+struct BfmePod52 { int a[13]; };
+inline bool operator==(const BfmePod52 &x, const BfmePod52 &y) { return x.a[0] == y.a[0]; }
+struct BfmePod60 { int a[15]; };
+inline bool operator==(const BfmePod60 &x, const BfmePod60 &y) { return x.a[0] == y.a[0]; }
+struct BfmePod72 { int a[18]; };
+inline bool operator==(const BfmePod72 &x, const BfmePod72 &y) { return x.a[0] == y.a[0]; }
 template class _STL::hash_set<int, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<int> >;
 template class _STL::hash_map<int, BfmePod16, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod16> > >;
 template class _STL::hash_map<int, BfmePod20, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod20> > >;
 template class _STL::hash_map<int, BfmePod24, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod24> > >;
 template class _STL::hash_map<int, BfmePod32, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod32> > >;
 template class _STL::hash_map<int, BfmePod48, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod48> > >;
+template class _STL::hash_map<int, BfmePod44, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod44> > >;
+template class _STL::hash_map<int, BfmePod52, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod52> > >;
+template class _STL::hash_map<int, BfmePod60, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod60> > >;
+template class _STL::hash_map<int, BfmePod72, _STL::hash<int>, _STL::equal_to<int>, _STL::allocator<_STL::pair<const int, BfmePod72> > >;
