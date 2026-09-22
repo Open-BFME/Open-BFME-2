43874af0a4bb8bd2df67387d8e314dc00bcb88cb
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-22 07:08:20 +0000

    Recover TurretAI isTryingToAimAtTarget (0x004D871F 67B) via BFME1 donor; state-machine offsets retail-proven

--- Code/GameEngine/Source/GameLogic/Object/Update/AIUpdateInterface_isWeaponSlotOnTurretAndAimingAtTarget.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdateInterface_isWeaponSlotOnTurretAndAimingAtTarget.cpp
@@ -9,6 +9,51 @@
 
 typedef bool Bool;
 
+typedef int StateID;
+
+enum
+{
+	INVALID_STATE_ID = 999999
+};
+
+enum TurretTargetType
+{
+	TARGET_NONE = 0,
+	TARGET_OBJECT = 1,
+	TARGET_POSITION = 2
+};
+
+enum TurretStateType
+{
+	TURRETAI_IDLE = 0,
+	TURRETAI_IDLESCAN = 1,
+	TURRETAI_AIM = 2
+};
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+class Object;
+
+// BFME2 retail layout, every offset read directly by the byte-true body:
+// the state machine pointer sits at +0x14, its current-state link at +0x04,
+// and the StateID four bytes into the state.
+struct TurretState
+{
+	char m_pad[4];
+	StateID m_stateID;
+};
+
+struct TurretStateMachine
+{
+	char m_pad[4];
+	const TurretState *m_currentState;
+};
+
 enum WeaponSlotType
 {
 	WEAPONSLOT_PRIMARY = 0,
@@ -24,6 +69,11 @@ struct TurretAI
 {
 	Bool isWeaponSlotOnTurret(WeaponSlotType wslot) const;
 	Bool isTryingToAimAtTarget(const class Object *victim) const;
+	TurretTargetType friend_getTurretTarget(Object *&obj, Coord3D &pos) const;
+
+private:
+	char m_pad[0x14];
+	const TurretStateMachine *m_turretStateMachine;
 };
 
 class AIUpdateInterface
@@ -45,5 +95,18 @@ Bool AIUpdateInterface::isWeaponSlotOnTurretAndAimingAtTarget(WeaponSlotType wsl
 			return m_turretAI[i]->isTryingToAimAtTarget(victim);
 		}
 	}
+	return false;
+}
+
+// ?isTryingToAimAtTarget@TurretAI@@QBE_NPBVObject@@@Z
+Bool TurretAI::isTryingToAimAtTarget(const Object *victim) const
+{
+	const TurretStateMachine *machine = m_turretStateMachine;
+	StateID sid = machine->m_currentState ? machine->m_currentState->m_stateID : INVALID_STATE_ID;
+
+	Object *obj;
+	Coord3D pos;
+	if (sid == TURRETAI_AIM && friend_getTurretTarget(obj, pos) == TARGET_OBJECT && obj == victim)
+		return true;
 	return false;
 }
