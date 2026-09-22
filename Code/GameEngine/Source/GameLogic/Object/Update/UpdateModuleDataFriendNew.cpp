187b0654e8afbf43a7128ee8a7457d4289c33bc6
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-22 02:09:10 +0200

    Recover 18 Update-module data factories
    Each friend_newModuleData is named by its addModule registration and byte-verifies against the standard factory shape; its ctor and buildFieldParse callees are pinned. QueueProductionExitUpdate, BroadcastStealthUpdate and SlavedUpdate differ from the template and are left for later.

--- Code/GameEngine/Source/GameLogic/Object/Update/UpdateModuleDataFriendNew.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/UpdateModuleDataFriendNew.cpp
@@ -393,3 +393,381 @@ ModuleData *TeleportSpecialAbilityUpdateModuleData::friend_newModuleData(INI *in
 		ini->initFromINIMultiProc(data, TeleportSpecialAbilityUpdateModuleData::buildFieldParse);
 	return reinterpret_cast<ModuleData *>(data);
 }
+
+class HeroModeSpecialAbilityUpdateModuleData
+{
+public:
+	HeroModeSpecialAbilityUpdateModuleData();
+	virtual ~HeroModeSpecialAbilityUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xD4 - 4];
+};
+
+// ?friend_newModuleData@HeroModeSpecialAbilityUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *HeroModeSpecialAbilityUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	HeroModeSpecialAbilityUpdateModuleData *data = new HeroModeSpecialAbilityUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, HeroModeSpecialAbilityUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class WeaponSetSpecialAbilityUpdateModuleData
+{
+public:
+	WeaponSetSpecialAbilityUpdateModuleData();
+	virtual ~WeaponSetSpecialAbilityUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xD0 - 4];
+};
+
+// ?friend_newModuleData@WeaponSetSpecialAbilityUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *WeaponSetSpecialAbilityUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	WeaponSetSpecialAbilityUpdateModuleData *data = new WeaponSetSpecialAbilityUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, WeaponSetSpecialAbilityUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class ScaleWallSpecialAbilityUpdateModuleData
+{
+public:
+	ScaleWallSpecialAbilityUpdateModuleData();
+	virtual ~ScaleWallSpecialAbilityUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xCC - 4];
+};
+
+// ?friend_newModuleData@ScaleWallSpecialAbilityUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *ScaleWallSpecialAbilityUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	ScaleWallSpecialAbilityUpdateModuleData *data = new ScaleWallSpecialAbilityUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, ScaleWallSpecialAbilityUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class FlingPassengerSpecialAbilityUpdateModuleData
+{
+public:
+	FlingPassengerSpecialAbilityUpdateModuleData();
+	virtual ~FlingPassengerSpecialAbilityUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xD8 - 4];
+};
+
+// ?friend_newModuleData@FlingPassengerSpecialAbilityUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *FlingPassengerSpecialAbilityUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	FlingPassengerSpecialAbilityUpdateModuleData *data = new FlingPassengerSpecialAbilityUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, FlingPassengerSpecialAbilityUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class DozerAIUpdateModuleData
+{
+public:
+	DozerAIUpdateModuleData();
+	virtual ~DozerAIUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x70 - 4];
+};
+
+// ?friend_newModuleData@DozerAIUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *DozerAIUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	DozerAIUpdateModuleData *data = new DozerAIUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, DozerAIUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class FadeAndDieOrnamentUpdateModuleData
+{
+public:
+	FadeAndDieOrnamentUpdateModuleData();
+	virtual ~FadeAndDieOrnamentUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x38 - 4];
+};
+
+// ?friend_newModuleData@FadeAndDieOrnamentUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *FadeAndDieOrnamentUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	FadeAndDieOrnamentUpdateModuleData *data = new FadeAndDieOrnamentUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, FadeAndDieOrnamentUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class StructureCollapseUpdateModuleData
+{
+public:
+	StructureCollapseUpdateModuleData();
+	virtual ~StructureCollapseUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xFC - 4];
+};
+
+// ?friend_newModuleData@StructureCollapseUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *StructureCollapseUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	StructureCollapseUpdateModuleData *data = new StructureCollapseUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, StructureCollapseUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class AnimalAIUpdateModuleData
+{
+public:
+	AnimalAIUpdateModuleData();
+	virtual ~AnimalAIUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x80 - 4];
+};
+
+// ?friend_newModuleData@AnimalAIUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *AnimalAIUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	AnimalAIUpdateModuleData *data = new AnimalAIUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, AnimalAIUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class WorkerAIUpdateModuleData
+{
+public:
+	WorkerAIUpdateModuleData();
+	virtual ~WorkerAIUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x94 - 4];
+};
+
+// ?friend_newModuleData@WorkerAIUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *WorkerAIUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	WorkerAIUpdateModuleData *data = new WorkerAIUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, WorkerAIUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class RespawnUpdateModuleData
+{
+public:
+	RespawnUpdateModuleData();
+	virtual ~RespawnUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x120 - 4];
+};
+
+// ?friend_newModuleData@RespawnUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *RespawnUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	RespawnUpdateModuleData *data = new RespawnUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, RespawnUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class SpecialDisguiseUpdateModuleData
+{
+public:
+	SpecialDisguiseUpdateModuleData();
+	virtual ~SpecialDisguiseUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xE0 - 4];
+};
+
+// ?friend_newModuleData@SpecialDisguiseUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *SpecialDisguiseUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	SpecialDisguiseUpdateModuleData *data = new SpecialDisguiseUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, SpecialDisguiseUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class StatusBitsUpgradeIfEldestKindofModuleData
+{
+public:
+	StatusBitsUpgradeIfEldestKindofModuleData();
+	virtual ~StatusBitsUpgradeIfEldestKindofModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x13C - 4];
+};
+
+// ?friend_newModuleData@StatusBitsUpgradeIfEldestKindofModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *StatusBitsUpgradeIfEldestKindofModuleData::friend_newModuleData(INI *ini)
+{
+	StatusBitsUpgradeIfEldestKindofModuleData *data = new StatusBitsUpgradeIfEldestKindofModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, StatusBitsUpgradeIfEldestKindofModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class PillageModuleData
+{
+public:
+	PillageModuleData();
+	virtual ~PillageModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x14 - 4];
+};
+
+// ?friend_newModuleData@PillageModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *PillageModuleData::friend_newModuleData(INI *ini)
+{
+	PillageModuleData *data = new PillageModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, PillageModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class TransitionDamageFXModuleData
+{
+public:
+	TransitionDamageFXModuleData();
+	virtual ~TransitionDamageFXModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x1040 - 4];
+};
+
+// ?friend_newModuleData@TransitionDamageFXModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *TransitionDamageFXModuleData::friend_newModuleData(INI *ini)
+{
+	TransitionDamageFXModuleData *data = new TransitionDamageFXModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, TransitionDamageFXModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class PorcupineFormationBodyModuleData
+{
+public:
+	PorcupineFormationBodyModuleData();
+	virtual ~PorcupineFormationBodyModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x70 - 4];
+};
+
+// ?friend_newModuleData@PorcupineFormationBodyModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *PorcupineFormationBodyModuleData::friend_newModuleData(INI *ini)
+{
+	PorcupineFormationBodyModuleData *data = new PorcupineFormationBodyModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, PorcupineFormationBodyModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class ProductionSpeedBonusModuleData
+{
+public:
+	ProductionSpeedBonusModuleData();
+	virtual ~ProductionSpeedBonusModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0x90 - 4];
+};
+
+// ?friend_newModuleData@ProductionSpeedBonusModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *ProductionSpeedBonusModuleData::friend_newModuleData(INI *ini)
+{
+	ProductionSpeedBonusModuleData *data = new ProductionSpeedBonusModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, ProductionSpeedBonusModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class WoundArrowUpdateModuleData
+{
+public:
+	WoundArrowUpdateModuleData();
+	virtual ~WoundArrowUpdateModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xD0 - 4];
+};
+
+// ?friend_newModuleData@WoundArrowUpdateModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *WoundArrowUpdateModuleData::friend_newModuleData(INI *ini)
+{
+	WoundArrowUpdateModuleData *data = new WoundArrowUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, WoundArrowUpdateModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+class FellBeastSwoopPowerModuleData
+{
+public:
+	FellBeastSwoopPowerModuleData();
+	virtual ~FellBeastSwoopPowerModuleData();
+	static void buildFieldParse(MultiIniFieldParse &parse);
+	static ModuleData *friend_newModuleData(INI *ini);
+
+private:
+	unsigned char m_pad[0xD0 - 4];
+};
+
+// ?friend_newModuleData@FellBeastSwoopPowerModuleData@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *FellBeastSwoopPowerModuleData::friend_newModuleData(INI *ini)
+{
+	FellBeastSwoopPowerModuleData *data = new FellBeastSwoopPowerModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, FellBeastSwoopPowerModuleData::buildFieldParse);
+	return reinterpret_cast<ModuleData *>(data);
+}
