4b5e7aa8b8e9cac96c528a573d54a6d587ae4cf5
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-22 10:59:15 +0000

    Recover BfmePartVID computeVID (0x00195BC0 21B) via G7 shard; retail add over toolchain inc

---
+++ Code/GameEngine/Source/Common/BfmeConv1372Compute.cpp
@@ -0,0 +1,30 @@
+// cl: /G7
+
+class BfmePartVID
+{
+public:
+	int bfmeComputeVID();
+
+private:
+	char m_pad00[8];
+	int m_bfme08;
+	char m_pad0C[0x0C];
+	int m_bfme18;
+	int m_bfme1C;
+};
+
+class BfmePartBVID
+{
+public:
+	int bfmeComputeBVID();
+
+private:
+	char m_pad00[0x0C];
+	int m_bfme0C;
+	int m_bfme10;
+};
+
+int BfmePartVID::bfmeComputeVID()
+{
+	return ((m_bfme1C + 1 - m_bfme18) * m_bfme08) * 4 + 0x20;
+}
