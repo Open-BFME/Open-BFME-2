a736ee0cbf4675f8a0f62038947c85b3878f2e86
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-24 19:47:43 +0000

    Recover 16 flag clearers (208B) across the E036 run via /G7 al narrowing

--- Code/GameEngine/Source/Common/GlobalFlagClearers_E036xx.cpp
+++ Code/GameEngine/Source/Common/GlobalFlagClearers_E036xx.cpp
@@ -28,81 +28,81 @@ extern unsigned int g_Va00E036F8;
 
 unsigned int Rva0078A9F1ClearFlag(void)
 {
-	return g_Va00E03600 &= 0xFFFFFF0xFEu;
+	return g_Va00E03600 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078AA08ClearFlag(void)
 {
-	return g_Va00E03620 &= 0xFFFFFF0xFEu;
+	return g_Va00E03620 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078AB49ClearFlag(void)
 {
-	return g_Va00E03644 &= 0xFFFFFF0xFEu;
+	return g_Va00E03644 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078ACF7ClearFlag(void)
 {
-	return g_Va00E0364C &= 0xFFFFFF0xFEu;
+	return g_Va00E0364C &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078AD20ClearFlag(void)
 {
-	return g_Va00E03658 &= 0xFFFFFF0xFEu;
+	return g_Va00E03658 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078ADBBClearFlag(void)
 {
-	return g_Va00E03660 &= 0xFFFFFF0xFEu;
+	return g_Va00E03660 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078AEC8ClearFlag(void)
 {
-	return g_Va00E0366C &= 0xFFFFFF0xFEu;
+	return g_Va00E0366C &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078AF1CClearFlag(void)
 {
-	return g_Va00E03674 &= 0xFFFFFF0xFEu;
+	return g_Va00E03674 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B0B6ClearFlag(void)
 {
-	return g_Va00E0367C &= 0xFFFFFF0xFEu;
+	return g_Va00E0367C &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B1B9ClearFlag(void)
 {
-	return g_Va00E03684 &= 0xFFFFFF0xFEu;
+	return g_Va00E03684 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B216ClearFlag(void)
 {
-	return g_Va00E0368C &= 0xFFFFFF0xFEu;
+	return g_Va00E0368C &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B3EFClearFlag(void)
 {
-	return g_Va00E03698 &= 0xFFFFFF0xFEu;
+	return g_Va00E03698 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B418ClearFlag(void)
 {
-	return g_Va00E036A4 &= 0xFFFFFF0xFEu;
+	return g_Va00E036A4 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B499ClearFlag(void)
 {
-	return g_Va00E036B0 &= 0xFFFFFF0xFEu;
+	return g_Va00E036B0 &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B54FClearFlag(void)
 {
-	return g_Va00E036EC &= 0xFFFFFF0xFEu;
+	return g_Va00E036EC &= 0xFFFFFFFEu;
 }
 
 unsigned int Rva0078B5B0ClearFlag(void)
 {
-	return g_Va00E036F8 &= 0xFFFFFF0xFEu;
+	return g_Va00E036F8 &= 0xFFFFFFFEu;
 }
 
