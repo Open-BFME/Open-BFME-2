9930e38b4de451e4820e78fc69fe78a68eeb76b1
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-24 01:16:58 +0000

    Recover ProcessAnimateWindowSpiral reverse 261B @0x005C5E80 via home donor shard with storedVel tail; home def removed

--- Code/GameEngine/Source/GameClient/GUI/ProcessAnimateWindow.cpp
+++ Code/GameEngine/Source/GameClient/GUI/ProcessAnimateWindow.cpp
@@ -1064,56 +1064,10 @@ Bool ProcessAnimateWindowSpiral::updateAnimateWindow( AnimateWindow *animWin )
 }
 
 //-----------------------------------------------------------------------------
-Bool ProcessAnimateWindowSpiral::reverseAnimateWindow( AnimateWindow *animWin )
-{
-	
-	if(!animWin)
-	{
-		DEBUG_ASSERTCRASH( animWin, ("animWin was passed into updateAnimateWindow as a NULL Pointer... bad bad bad!"));
-		return TRUE;
-	}
 
-	// if the window has finished animating into position, return
-	if(animWin->isFinished())
-		return TRUE;
-
-
-
-	// it's set that the window is passed in as it's current position being it's rest position
-	// so save off the rest position
-	GameWindow *win = animWin->getGameWindow();
-	if(!win)
-	{
-		DEBUG_ASSERTCRASH( win, ("animWin contains a NULL Pointer for it's GameWindow... Whatup wit dat?"));
-		return TRUE;
-	}
-
-	ICoord2D curPos = animWin->getCurPos();
-	ICoord2D endPos = animWin->getEndPos();
-	Coord2D vel = animWin->getVel();
-	
-	curPos.x = (vel.y * cos(vel.x)) + endPos.x; 
-	curPos.y = (vel.y * sin(vel.x)) + endPos.y;
-
-	vel.x = vel.x - m_deltaTheta;
-	vel.y +=5;
-	
-	ICoord2D size;
-	win->winGetSize(&size.x, &size.y);
-//	Int m_max = min(size.x/2, size.y/2);
-
-	if(vel.y > m_maxR)
-	{
-		//ICoord2D restPos = animWin->getRestPos();
-		animWin->setFinished( TRUE );
-		//win->winSetPosition(restPos.x, restPos.y);
-		return TRUE;
-	}
-	win->winSetPosition(curPos.x, curPos.y);
-	animWin->setCurPos(curPos);
-	animWin->setVel(vel);
-	return FALSE;
-}
+// NOTE: Spiral::reverseAnimateWindow lives in
+// ProcessAnimateWindowSpiralReverse.cpp (it needs /O1 for the framed
+// manual prologue plus storedVel tail; this /G7 TU emits integer tail).
 
 
 //-----------------------------------------------------------------------------
