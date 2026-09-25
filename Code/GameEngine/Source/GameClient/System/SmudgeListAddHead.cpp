7eac7f8dfcf637e116d5570933df2ecffa2299da
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-25 18:37:52 +0000

    Recover SmudgeSet node Remove 117B

--- Code/GameEngine/Source/GameClient/System/SmudgeListAddHead.cpp
+++ Code/GameEngine/Source/GameClient/System/SmudgeListAddHead.cpp
@@ -126,6 +126,7 @@ template <class T> void DLNodeClass<T>::Remove()
 }
 
 template void DLNodeClass<Smudge>::Remove();
+template void DLNodeClass<SmudgeSet>::Remove();
 
 template <class T> void DLListClass<T>::Remove_Tail()
 {
