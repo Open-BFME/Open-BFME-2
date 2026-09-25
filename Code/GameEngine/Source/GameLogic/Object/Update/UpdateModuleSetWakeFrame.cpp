01e06978c32ea2a3ad7c5fc14d6a078bbef8c24c
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-25 23:27:11 +0000

    Recover UpdateModule::getWakeFrame 22B @0x0044DF5B
    Abuts frameToSleepTime and setWakeFrame; reads the GameLogic frame plus
    the raw +0x14 next-call word (BFME2 dropped the donor phase shift) and
    returns the delta or UPDATE_SLEEP_NONE.

--- Code/GameEngine/Source/GameLogic/Object/Update/UpdateModuleSetWakeFrame.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/UpdateModuleSetWakeFrame.cpp
@@ -11,8 +11,14 @@ typedef unsigned int UnsignedInt;
 
 enum UpdateSleepTime
 {
+	UPDATE_SLEEP_INVALID = 0,
+	UPDATE_SLEEP_NONE = 1,
+	// (we use 0x3fffffff so that we can add offsets and not overflow...
+	//	and also 'cuz we shift the value up by two bits for the phase.
+	// note that at 30fps, this is ~414 days...
 	UPDATE_SLEEP_FOREVER = 0x3fffffff
 };
+#define UPDATE_SLEEP(numFrames) ((UpdateSleepTime)(numFrames))
 
 class Object;
 class UpdateModule;
@@ -30,10 +36,40 @@ private:
 
 extern GameLogic *TheGameLogic;
 
-class UpdateModule
+class Thing;
+class ModuleData;
+
+class BehaviorModuleBase
 {
+	virtual void unused();
+	int a;
+	int b;
+};
+
+class BehaviorModuleOther
+{
+	virtual void unused();
+};
+
+class BehaviorModule : public BehaviorModuleBase, public BehaviorModuleOther
+{
+public:
+	BehaviorModule(Thing *thing, const ModuleData *moduleData);
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void update() = 0;
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+	UnsignedInt m_nextCallFrameAndPhase; // +0x14 (ctor TU precedent)
+
 protected:
 	void setWakeFrame(Object *obj, UpdateSleepTime wakeDelay);
+	UpdateSleepTime getWakeFrame() const;
 };
 
 // ?setWakeFrame@UpdateModule@@IAEXPAVObject@@W4UpdateSleepTime@@@Z @0x0044DF71
@@ -41,4 +77,17 @@ void UpdateModule::setWakeFrame(Object *obj, UpdateSleepTime wakeDelay)
 {
 	UnsignedInt now = TheGameLogic->getFrame();
 	TheGameLogic->friend_awakenUpdateModule(obj, this, now + (UnsignedInt)wakeDelay);
+}
+
+// ?getWakeFrame@UpdateModule@@IBE?AW4UpdateSleepTime@@XZ @0x0044DF5B
+// BFME1 donor reads (m_nextCallFrameAndPhase >> 2); retail compares the raw
+// +0x14 word, so BFME2 dropped the phase shift here.
+UpdateSleepTime UpdateModule::getWakeFrame() const
+{
+	UnsignedInt now = TheGameLogic->getFrame();
+	UnsignedInt nextCallFrame = m_nextCallFrameAndPhase;
+	if (nextCallFrame > now)
+		return UPDATE_SLEEP(nextCallFrame - now);
+	else
+		return UPDATE_SLEEP_NONE;
 }
