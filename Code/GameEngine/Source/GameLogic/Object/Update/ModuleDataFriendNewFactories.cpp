472de2b4830765c79c3671151170a0fc7bede0e7
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-25 18:37:02 +0000

    Recover 8 ModuleData friend_newModuleData factories 648B

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/ModuleDataFriendNewFactories.cpp
@@ -0,0 +1,215 @@
+// cl: /O1 /GX /DNDEBUG /MD
+//
+// Same-shape siblings of ?friend_newModuleData@W3DLaserDraw (0x00064962,
+// 81 bytes; see W3DLaserDrawModuleDataFriendNew.cpp). Each factory news its
+// ModuleData subclass (declared-only ctor resolving through that ctor's own
+// row), then feeds the new data plus its class parse proc (pushed immediate,
+// auto-filled by the gate, no pin) to INI::initFromINIMultiProc (rowed at
+// 0x2DEB5) when ini is non-null. Operator new and __EH_prolog resolve via
+// their rows. Owner classes are Rva-named: the real owners are unknown (the
+// factories are referenced only by address from the module registration
+// tables), so each claims a descriptive Rva owner rather than guessing.
+
+class ModuleData;
+class INI;
+class MultiIniFieldParse;
+
+class INI
+{
+public:
+	void initFromINIMultiProc(void *what, void (__cdecl *proc)(MultiIniFieldParse &));
+};
+
+// 0x0024AF5C: news 0x0C, ctor ??0LockWeaponCreateModuleData (rowed 0x254C8E),
+// parse proc 0x8598E1.
+void LockWeaponCreateModuleDataParse_24AF5C(MultiIniFieldParse &parse);
+
+class LockWeaponCreateModuleData
+{
+public:
+	LockWeaponCreateModuleData();
+	virtual ~LockWeaponCreateModuleData();
+
+private:
+	unsigned char m_pad[0x0C - 4];
+};
+
+class Rva0024AF5C
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@Rva0024AF5C@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva0024AF5C::friend_newModuleData(INI *ini)
+{
+	LockWeaponCreateModuleData *data = new LockWeaponCreateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, LockWeaponCreateModuleDataParse_24AF5C);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// 0x0024CF56: news 0x10, ctor ??0Rva0048BC03Base (rowed 0x48BC03),
+// parse proc 0x88C0B4.
+void Rva0048BC03BaseParse_24CF56(MultiIniFieldParse &parse);
+
+class Rva0048BC03Base
+{
+public:
+	Rva0048BC03Base();
+	virtual ~Rva0048BC03Base();
+
+private:
+	unsigned char m_pad[0x10 - 4];
+};
+
+class Rva0024CF56
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@Rva0024CF56@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva0024CF56::friend_newModuleData(INI *ini)
+{
+	Rva0048BC03Base *data = new Rva0048BC03Base;
+	if (ini)
+		ini->initFromINIMultiProc(data, Rva0048BC03BaseParse_24CF56);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// 0x0024FC08: news 0x0C, ctor ??0RadarUpdateModuleData (rowed 0x3ECC80),
+// parse proc 0x7ECC91.
+void RadarUpdateModuleDataParse_24FC08(MultiIniFieldParse &parse);
+
+class RadarUpdateModuleData
+{
+public:
+	RadarUpdateModuleData();
+	virtual ~RadarUpdateModuleData();
+
+private:
+	unsigned char m_pad[0x0C - 4];
+};
+
+class Rva0024FC08
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@Rva0024FC08@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva0024FC08::friend_newModuleData(INI *ini)
+{
+	RadarUpdateModuleData *data = new RadarUpdateModuleData;
+	if (ini)
+		ini->initFromINIMultiProc(data, RadarUpdateModuleDataParse_24FC08);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// 0x002524B2 / 0x002526DF / 0x00252768 / 0x0025287D: all news 0x7C with ctor
+// ??0DeliverPayloadAIUpdate (rowed 0x4C72D3); parse procs 0x8C7AEB,
+// 0x8C856F, 0x8C8699, 0x8C89CB.
+void DeliverPayloadAIUpdateParse_2524B2(MultiIniFieldParse &parse);
+void DeliverPayloadAIUpdateParse_2526DF(MultiIniFieldParse &parse);
+void DeliverPayloadAIUpdateParse_252768(MultiIniFieldParse &parse);
+void DeliverPayloadAIUpdateParse_25287D(MultiIniFieldParse &parse);
+
+class DeliverPayloadAIUpdate
+{
+public:
+	DeliverPayloadAIUpdate();
+	virtual ~DeliverPayloadAIUpdate();
+
+private:
+	unsigned char m_pad[0x7C - 4];
+};
+
+class Rva002524B2
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+class Rva002526DF
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+class Rva00252768
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+class Rva0025287D
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@Rva002524B2@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva002524B2::friend_newModuleData(INI *ini)
+{
+	DeliverPayloadAIUpdate *data = new DeliverPayloadAIUpdate;
+	if (ini)
+		ini->initFromINIMultiProc(data, DeliverPayloadAIUpdateParse_2524B2);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// ?friend_newModuleData@Rva002526DF@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva002526DF::friend_newModuleData(INI *ini)
+{
+	DeliverPayloadAIUpdate *data = new DeliverPayloadAIUpdate;
+	if (ini)
+		ini->initFromINIMultiProc(data, DeliverPayloadAIUpdateParse_2526DF);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// ?friend_newModuleData@Rva00252768@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva00252768::friend_newModuleData(INI *ini)
+{
+	DeliverPayloadAIUpdate *data = new DeliverPayloadAIUpdate;
+	if (ini)
+		ini->initFromINIMultiProc(data, DeliverPayloadAIUpdateParse_252768);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// ?friend_newModuleData@Rva0025287D@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva0025287D::friend_newModuleData(INI *ini)
+{
+	DeliverPayloadAIUpdate *data = new DeliverPayloadAIUpdate;
+	if (ini)
+		ini->initFromINIMultiProc(data, DeliverPayloadAIUpdateParse_25287D);
+	return reinterpret_cast<ModuleData *>(data);
+}
+
+// 0x00252C13: news 0x14, ctor ??0BaikonurLaunchPower (rowed 0x4CAEE6),
+// parse proc 0x8CB0F3.
+void BaikonurLaunchPowerParse_252C13(MultiIniFieldParse &parse);
+
+class BaikonurLaunchPower
+{
+public:
+	BaikonurLaunchPower();
+	virtual ~BaikonurLaunchPower();
+
+private:
+	unsigned char m_pad[0x14 - 4];
+};
+
+class Rva00252C13
+{
+public:
+	static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@Rva00252C13@@SAPAVModuleData@@PAVINI@@@Z
+ModuleData *Rva00252C13::friend_newModuleData(INI *ini)
+{
+	BaikonurLaunchPower *data = new BaikonurLaunchPower;
+	if (ini)
+		ini->initFromINIMultiProc(data, BaikonurLaunchPowerParse_252C13);
+	return reinterpret_cast<ModuleData *>(data);
+}
