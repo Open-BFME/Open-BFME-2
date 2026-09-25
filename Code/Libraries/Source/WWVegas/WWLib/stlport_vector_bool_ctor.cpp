6944dc49c9906585a084c2d480af2f6c78ff78c2
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-25 19:17:24 +0000

    Recover vector<bool> fill ctor 79B @0x0043FE13

---
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_vector_bool_ctor.cpp
@@ -0,0 +1,6 @@
+// cl: /O1 /Ob2 /EHs /MD /D_CRTIMP= /D_STLP_USE_STATIC_LIB
+// stlport
+// STLport 4.5.3 bit-vector fill constructor used by retail 0x0043FE13.
+// Out-of-line base plus _M_initialize with an inlined 0/-1 fill loop.
+#include <vector>
+template _STL::vector<bool>::vector(unsigned, bool, _STL::allocator<bool> const &);
