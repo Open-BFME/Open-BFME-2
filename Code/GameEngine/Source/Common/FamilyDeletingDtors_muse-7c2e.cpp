35e7f709eaed7e2c2d41f111e0502b947a8225c4
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:27:06 +0000

    Recover CommandPointsUpgradeModuleData deleting dtor 28B @0x0025593D via slot0 rowed dtor

---
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-7c2e.cpp
@@ -0,0 +1,7 @@
+// cl: /O1 /MD
+// ??_GCommandPointsUpgradeModuleData@@UAEPAXI@Z @0x0025593D 28B
+// Deleting dtor slot 0 of vtable 0x007F2658; calls rowed ??1 at 0x00255959 then rowed operator delete at 0x0002FD60.
+class CommandPointsUpgradeModuleData { public: __declspec(noinline) virtual ~CommandPointsUpgradeModuleData(); private: int m_famgen; };
+// ??1CommandPointsUpgradeModuleData@@UAE@XZ present-unmatched
+CommandPointsUpgradeModuleData::~CommandPointsUpgradeModuleData() { m_famgen = 0; }
+void famgenDeleteCommandPointsUpgradeModuleData(CommandPointsUpgradeModuleData *p) { delete p; }
