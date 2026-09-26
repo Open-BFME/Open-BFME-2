eb8ff0ea3b0e99b15db5e610564f7f4019112058
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:46:10 +0000

    Recover AttachUpdateModuleData deleting dtor 28B @0x00491A84 via slot 0 deleting dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-p7z-f52d.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-p7z-f52d.cpp
@@ -4,3 +4,8 @@
 class MonitorConditionUpdateModuleData { public: __declspec(noinline) virtual ~MonitorConditionUpdateModuleData(); private: int m_famgen; };
 MonitorConditionUpdateModuleData::~MonitorConditionUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(MonitorConditionUpdateModuleData *p) { delete p; }
+// ??_GAttachUpdateModuleData@@UAEPAXI@Z @0x00491A84 28B
+// Deleting dtor slot 0 of vtable 0x0084DD18; calls rowed ??1 at 0x00491AA0 then rowed operator delete at 0x0002FD60.
+class AttachUpdateModuleData { public: __declspec(noinline) virtual ~AttachUpdateModuleData(); private: int m_famgen; };
+AttachUpdateModuleData::~AttachUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(AttachUpdateModuleData *p) { delete p; }
