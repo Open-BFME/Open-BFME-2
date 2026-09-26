cb02c3ee3dc7e900216a7e0fb42512f616434b2e
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:28:54 +0000

    Recover MonitorConditionUpdate deleting dtor 28B @0x00491750 via family TU

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-2c79.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-2c79.cpp
@@ -5,3 +5,9 @@ class AnimateWindowManager { public: __declspec(noinline) virtual ~AnimateWindow
 // ??1AnimateWindowManager@@UAE@XZ present-unmatched
 AnimateWindowManager::~AnimateWindowManager() { m_famgen = 0; }
 void famgenDeleteAnimateWindowManager(AnimateWindowManager *p) { delete p; }
+// ??_GMonitorConditionUpdate@@UAEPAXI@Z @0x00491750 28B
+// Deleting dtor slot 0 of vtable 0x0084DAD8; calls rowed ??1 at 0x004916AC then rowed operator delete at 0x0002FD60.
+class MonitorConditionUpdate { public: __declspec(noinline) virtual ~MonitorConditionUpdate(); private: int m_famgen; };
+// ??1MonitorConditionUpdate@@UAE@XZ present-unmatched
+MonitorConditionUpdate::~MonitorConditionUpdate() { m_famgen = 0; }
+void famgenDeleteMonitorConditionUpdate(MonitorConditionUpdate *p) { delete p; }
