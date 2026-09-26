886d67dd32a62f97bb78379bbaaa350749073a71
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:45:28 +0000

    Recover AODCrushCollideModuleData deleting dtor 28B @0x004BC06C via slot0 deleting dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-95ab.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-95ab.cpp
@@ -4,3 +4,9 @@
 class DominateEnemySpecialPowerModuleData { public: __declspec(noinline) virtual ~DominateEnemySpecialPowerModuleData(); private: int m_famgen; };
 DominateEnemySpecialPowerModuleData::~DominateEnemySpecialPowerModuleData() { m_famgen = 0; }
 void famgenDelete(DominateEnemySpecialPowerModuleData *p) { delete p; }
+
+// ??_GAODCrushCollideModuleData@@UAEPAXI@Z @0x004BC06C 28B
+// Deleting dtor slot 0 of vtable 0x0085A350; calls rowed ??1 at 0x004BC088 then rowed operator delete at 0x0002FD60.
+class AODCrushCollideModuleData { public: __declspec(noinline) virtual ~AODCrushCollideModuleData(); private: int m_famgen; };
+AODCrushCollideModuleData::~AODCrushCollideModuleData() { m_famgen = 0; }
+void famgenDelete(AODCrushCollideModuleData *p) { delete p; }
