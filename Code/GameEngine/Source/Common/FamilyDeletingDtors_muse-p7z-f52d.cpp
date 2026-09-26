000ab6e4fbad1b871176b0a840d604337f282baa
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:50:06 +0000

    Recover PickupStuffUpdateModuleData deleting dtor 28B @0x00491F5A via slot 0 deleting dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-p7z-f52d.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-p7z-f52d.cpp
@@ -9,3 +9,8 @@ void famgenDelete(MonitorConditionUpdateModuleData *p) { delete p; }
 class AttachUpdateModuleData { public: __declspec(noinline) virtual ~AttachUpdateModuleData(); private: int m_famgen; };
 AttachUpdateModuleData::~AttachUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(AttachUpdateModuleData *p) { delete p; }
+// ??_GPickupStuffUpdateModuleData@@UAEPAXI@Z @0x00491F5A 28B
+// Deleting dtor slot 0 of vtable 0x0084DDE0; calls rowed ??1 at 0x00491F76 then rowed operator delete at 0x0002FD60.
+class PickupStuffUpdateModuleData { public: __declspec(noinline) virtual ~PickupStuffUpdateModuleData(); private: int m_famgen; };
+PickupStuffUpdateModuleData::~PickupStuffUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(PickupStuffUpdateModuleData *p) { delete p; }
