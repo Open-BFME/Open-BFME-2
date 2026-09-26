b798825d27303a674e615bb4a9254b8254fe9637
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 14:53:30 +0000

    Recover EnragedBehavior::xfer 39B @0x00459047 via Version1 plus base plus float

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/EnragedBehaviorXfer.cpp
@@ -0,0 +1,116 @@
+// cl: /O1 /DNDEBUG /MD
+//
+// ?xfer@EnragedBehavior@@MAEXPAVXfer@@@Z, retail 0x00459047, 39 bytes.
+// Slot 3 (offset 0x0C) of vtable 0x00840FBC: Version1 via rowed 0x000053EE,
+// base UpdateModule xfer via rowed 0x0044DF9F, then float at +0x20 via Xfer
+// slot 0x70. Layout is the rowed EnragedBehavior class (float m_20).
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+class Coord3DBase;
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+class UpdateModule
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~UpdateModule();
+	void xfer(Xfer *xfer);
+
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+	const void *m_p0C;
+	const void *m_p10;
+	unsigned char m_pad[0x20 - 0x14];
+};
+
+class EnragedBehavior : public UpdateModule
+{
+public:
+	EnragedBehavior(Thing *thing, const ModuleData *moduleData);
+
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	float m_20;
+};
+
+void EnragedBehavior::xfer(Xfer *xfer)
+{
+	xfer->Version1();
+	UpdateModule::xfer(xfer);
+	*xfer == m_20;
+}
