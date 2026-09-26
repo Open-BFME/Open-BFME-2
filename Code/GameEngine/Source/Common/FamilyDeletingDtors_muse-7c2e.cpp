d2bb4077190c87aa8b0daadb0f796e4a01d7eb72
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:29:30 +0000

    Recover RefundDieModuleData deleting dtor 28B @0x0025504B via slot0 rowed dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-7c2e.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-7c2e.cpp
@@ -5,3 +5,9 @@ class CommandPointsUpgradeModuleData { public: __declspec(noinline) virtual ~Com
 // ??1CommandPointsUpgradeModuleData@@UAE@XZ present-unmatched
 CommandPointsUpgradeModuleData::~CommandPointsUpgradeModuleData() { m_famgen = 0; }
 void famgenDeleteCommandPointsUpgradeModuleData(CommandPointsUpgradeModuleData *p) { delete p; }
+// ??_GRefundDieModuleData@@UAEPAXI@Z @0x0025504B 28B
+// Deleting dtor slot 0 of vtable 0x007F0660; calls rowed ??1 at 0x00255067 then rowed operator delete at 0x0002FD60.
+class RefundDieModuleData { public: __declspec(noinline) virtual ~RefundDieModuleData(); private: int m_famgen; };
+// ??1RefundDieModuleData@@UAE@XZ present-unmatched
+RefundDieModuleData::~RefundDieModuleData() { m_famgen = 0; }
+void famgenDeleteRefundDieModuleData(RefundDieModuleData *p) { delete p; }
