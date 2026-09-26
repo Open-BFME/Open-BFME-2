48eebc079dc887b8681a4dc1e0f73a07f156c1d5
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:17:33 +0000

    Recover SpellRechargeModifierUpgradeModuleData deleting dtor 28B @0x004B606E via family ??_G pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-63c1.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-63c1.cpp
@@ -4,3 +4,8 @@
 class GeometryUpgrade { public: __declspec(noinline) virtual ~GeometryUpgrade(); private: int m_famgen; };
 GeometryUpgrade::~GeometryUpgrade() { m_famgen = 0; }
 void famgenDelete(GeometryUpgrade *p) { delete p; }
+// ??_GSpellRechargeModifierUpgradeModuleData@@UAEPAXI@Z @0x004B606E 28B
+// Deleting dtor slot 0 of vtable 0x008583E0; calls rowed ??1SpellRechargeModifierUpgradeModuleData@@UAE@XZ at 0x004B608A then rowed operator delete at 0x0002FD60.
+class SpellRechargeModifierUpgradeModuleData { public: __declspec(noinline) virtual ~SpellRechargeModifierUpgradeModuleData(); private: int m_famgen; };
+SpellRechargeModifierUpgradeModuleData::~SpellRechargeModifierUpgradeModuleData() { m_famgen = 0; }
+void famgenDelete(SpellRechargeModifierUpgradeModuleData *p) { delete p; }
