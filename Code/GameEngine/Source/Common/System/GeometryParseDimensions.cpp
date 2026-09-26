bc408d6f0e09bcb8ad31d1918a504491a13333a6
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 01:32:08 +0000

    Recover GeometryUsedForHealthBox parser 63B @0x006BD5D0 via last-shape bool

--- Code/GameEngine/Source/Common/System/GeometryParseDimensions.cpp
+++ Code/GameEngine/Source/Common/System/GeometryParseDimensions.cpp
@@ -12,7 +12,7 @@ public:
 };
 struct GeometryShape {
     int type; float height, majorRadius, minorRadius, x, y, z;
-    char *name; bool active; char padding[3];
+    char *name; bool active; bool usedForHealthBox; char padding[2];
 };
 class GeometryInfo {
 public:
@@ -20,6 +20,7 @@ public:
     static void parseGeometryMinorRadius(INI *, void *, void *, const void *);
     static void parseGeometryHeight(INI *, void *, void *, const void *);
     static void parseGeometryIsSmall(INI *, void *, void *, const void *);
+    static void parseGeometryUsedForHealthBox(INI *, void *, void *, const void *);
     void calcBoundingStuff();
     char unmodelled00[4]; bool isSmall; char unmodelled05[0x27];
     std::vector<GeometryShape> shapes;
@@ -55,4 +56,12 @@ void GeometryInfo::parseGeometryIsSmall(INI *ini, void *, void *store, const voi
     GeometryInfo *geometry=(GeometryInfo *)store;
     geometry->isSmall=ini->scanBool(ini->getNextToken(0));
     geometry->calcBoundingStuff();
+}
+
+// Table 0x80EBE0 GeometryUsedForHealthBox entry. Unlike the dimension
+// siblings it stores into the last shape without a bounding refresh.
+void GeometryInfo::parseGeometryUsedForHealthBox(INI *ini, void *, void *store, const void *) {
+    GeometryInfo *geometry=(GeometryInfo *)store;
+    if(geometry->shapes.size()!=0)
+        geometry->shapes.back().usedForHealthBox=ini->scanBool(ini->getNextToken(0));
 }
