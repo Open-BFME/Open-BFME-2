9d34a033f995bf1877e4b42944c2307af4178721
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:49:21 +0000

    Recover Rva004A3F8B::xfer 102B @0x004A3FAA via slot-3 xfer pattern

---
+++ Code/GameEngine/Source/Common/Rva004A3F8BXfer.cpp
@@ -0,0 +1,154 @@
+// cl: /O1 /MD
+//
+// ?xfer@Rva004A3F8B@@MAEXPAVXfer@@@Z, retail 0x004A3FAA, 102 bytes. Virtual
+// slot 3 (offset 0x0C) of vtable 0x008525E4 (class of rowed dtor
+// ??1Rva004A3F8B@@UAE@XZ in Rva0024A797Derived.cpp): base UpdateModule xfer
+// via rowed 0x0044DF9F then IsLightCRC early-out via Xfer slot 0x10 then
+// Version1 via rowed 0x000053EE then ObjectID at +0x20 via rowed
+// XferObjectID 0x003060B2 then Coord at +0x24 via Xfer slot 0x60 then bools
+// at +0x30/+0x31/+0x32 via Xfer slot 0x90. Layout is UpdateModule base 0x20
+// giving +0x20 start. Identity is slot 3 plus the UpdateModule base call;
+// class name stays honest Rva address name.
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+class Coord3DBase;
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+struct Coord3DBase
+{
+	float x;
+	float y;
+	float z;
+};
+
+enum ObjectID
+{
+	INVALID_ID = 0
+};
+
+void XferObjectID(Xfer *xfer, ObjectID *objectID);
+
+class ObjectModule
+{
+public:
+	ObjectModule(Thing *, const ModuleData *);
+	virtual ~ObjectModule();
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+
+class BehaviorModuleInterface { public: virtual void behaviorSlot(); };
+class UpdateModuleInterface { public: virtual void updateSlot(); };
+
+class UpdateModule : public ObjectModule,
+	public BehaviorModuleInterface, public UpdateModuleInterface
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *data);
+	virtual ~UpdateModule();
+	void xfer(Xfer *xfer);
+
+private:
+	unsigned int m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	int m_updateState;
+};
+
+class Rva004A3F8B : public UpdateModule
+{
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	ObjectID m_20;
+	Coord3DBase m_24;
+	bool m_30;
+	bool m_31;
+	bool m_32;
+};
+
+void Rva004A3F8B::xfer(Xfer *xfer)
+{
+	UpdateModule::xfer(xfer);
+	if (xfer->IsLightCRC())
+		return;
+	xfer->Version1();
+	XferObjectID(xfer, &m_20);
+	*xfer == m_24;
+	*xfer == m_30;
+	*xfer == m_31;
+	*xfer == m_32;
+}
