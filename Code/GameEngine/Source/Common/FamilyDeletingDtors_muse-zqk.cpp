f3b01a7e3be1f756b17fafc115af0f4150e2acff
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:49:06 +0000

    Recover HeroModeSpecialAbilityUpdateModuleData deleting dtor 28B @0x004922D5 via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-zqk.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-zqk.cpp
@@ -3,3 +3,7 @@
 class CivilianSpawnCollideModuleData { public: __declspec(noinline) virtual ~CivilianSpawnCollideModuleData(); private: int m_famgen; };
 CivilianSpawnCollideModuleData::~CivilianSpawnCollideModuleData() { m_famgen = 0; }
 void famgenDelete(CivilianSpawnCollideModuleData *p) { delete p; }
+// ??_GHeroModeSpecialAbilityUpdateModuleData@@UAEPAXI@Z @0x004922D5
+class HeroModeSpecialAbilityUpdateModuleData { public: __declspec(noinline) virtual ~HeroModeSpecialAbilityUpdateModuleData(); private: int m_famgen; };
+HeroModeSpecialAbilityUpdateModuleData::~HeroModeSpecialAbilityUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(HeroModeSpecialAbilityUpdateModuleData *p) { delete p; }
