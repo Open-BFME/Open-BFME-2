66305ff98f517e1fa4bc2d674be3c6b2dd011d3d
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 09:55:37 +0000

    Recover CommandPointsUpgrade deleting dtor 28B @0x004B8692 via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-d57a.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-d57a.cpp
@@ -5,3 +5,9 @@ class QueueProductionExitUpdate { protected: __declspec(noinline) virtual ~Queue
   friend void famgenDelete(QueueProductionExitUpdate *p); };
 QueueProductionExitUpdate::~QueueProductionExitUpdate() { m_famgen = 0; }
 void famgenDelete(QueueProductionExitUpdate *p) { delete p; }
+
+// ??_GCommandPointsUpgrade@@UAEPAXI@Z @0x004B8692
+// Deleting dtor slot 0 of vtable 0x008590A8; calls the rowed ??1 at 0x004B85D4.
+class CommandPointsUpgrade { public: __declspec(noinline) virtual ~CommandPointsUpgrade(); private: int m_famgen; };
+CommandPointsUpgrade::~CommandPointsUpgrade() { m_famgen = 0; }
+void famgenDelete(CommandPointsUpgrade *p) { delete p; }
