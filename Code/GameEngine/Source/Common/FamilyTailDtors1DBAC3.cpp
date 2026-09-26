8eb267cb3a88e7d90b3f4e946b161f7f75aaebe4
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:44:09 +0000

    Recover Rva0035E00F::Rva0035E0F7 142B @0x0035E0F7 via range hide variant

--- Code/GameEngine/Source/Common/FamilyTailDtors1DBAC3.cpp
+++ Code/GameEngine/Source/Common/FamilyTailDtors1DBAC3.cpp
@@ -9,6 +9,10 @@
 // vslot 0x0035DD08 (?Rva0035DD08@Rva0035DCF9@@UAEXH@Z, 148B, slot 2 offset 0x8
 // of vtable 0x00816574): range-select hide via rowed winHide 0x00313C64;
 // sets +0x28 to -1 then to index in middle range. Evidence: vtable 0x00816574.
+// vslot 0x0035E0F7 (?Rva0035E0F7@Rva0035E00F@@UAEXH@Z, 142B, slot 2 offset 0x8
+// of vtable 0x00816594): same range-select shape with second window at +0x4C
+// (not +0x44); first equality branch hides only m_44. Evidence: vtable
+// 0x00816594 slot 2 plus rowed winHide 0x00313C64.
 
 class Rva001DBAC3Base { public: virtual ~Rva001DBAC3Base(); };
 
@@ -97,9 +101,62 @@ void Rva0035DCF9::Rva0035DD08(int index)
 	m_28 = index;
 }
 
-class Rva0035E00F : public Rva001DBAC3Base { public: virtual ~Rva0035E00F(); int m_pad[2]; int m_field; };
-Rva0035E00F::~Rva0035E00F() { m_field = 0; }
+class Rva0035E00F : public Rva001DBAC3Base
+{
+public:
+	virtual ~Rva0035E00F();
+	virtual void slot1();
+	virtual void Rva0035E0F7(int index);
+	virtual void slot3();
+	virtual void slot4();
+	virtual void slot5();
+	virtual void slot6();
+	virtual void slot7();
+private:
+	char m_pad4[4];
+	bool m_8;
+	bool m_9;
+	char m_padA[2];
+	GameWindow *m_C;
+	int m_10;
+	int m_14;
+	char m_pad18[0x28 - 0x18];
+	int m_28;
+	char m_pad2C[0x4C - 0x2C];
+	GameWindow *m_44;
+};
+Rva0035E00F::~Rva0035E00F() { m_C = 0; }
 void famgenDelete(Rva0035E00F *p) { delete p; }
+
+void Rva0035E00F::Rva0035E0F7(int index)
+{
+	m_28 = -1;
+	if (index < m_10)
+		return;
+	if (index > m_14)
+		return;
+	if (index == m_10) {
+		if (!m_9 && m_C != 0 && m_44 != 0) {
+			m_44->winHide(true);
+			m_8 = true;
+		}
+	} else if (index == m_14) {
+		if (m_9 && m_C != 0 && m_44 != 0) {
+			m_C->winHide(true);
+			m_44->winHide(false);
+			m_8 = true;
+		}
+	}
+	if (index <= m_10)
+		return;
+	if (index >= m_14)
+		return;
+	if (m_C != 0)
+		m_C->winHide(true);
+	if (m_44 != 0)
+		m_44->winHide(true);
+	m_28 = index;
+}
 
 class Rva0035E2CF : public Rva001DBAC3Base { public: virtual ~Rva0035E2CF(); int m_pad[2]; int m_field; };
 Rva0035E2CF::~Rva0035E2CF() { m_field = 0; }
