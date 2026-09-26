6267bdf668faef8c344784a413b7e3827fd880d1
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:12:49 +0000

    Recover RAMFile::scanString 128B @0x006056FA via ZH with temp1 concat

--- Code/GameEngine/Source/Common/System/RAMFileRead.cpp
+++ Code/GameEngine/Source/Common/System/RAMFileRead.cpp
@@ -9,12 +9,47 @@
 #define NULL 0
 
 extern "C" void *__cdecl memcpy(void *dst, const void *src, unsigned int n);
+extern "C" __declspec(dllimport) int __cdecl isspace(int c);
+
+template <typename T> class StringBase
+{
+	friend class AsciiString;
+
+private:
+	struct Header
+	{
+		int refCount;
+		unsigned short length;
+		unsigned short capacity;
+		T data[1];
+	};
+
+	Header *m_data;
+
+	StringBase() : m_data(0) {}
+	void releaseBuffer();
+
+public:
+	void concat(const T *text, int length);
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	void clear();
+
+	void concat(const char *text, int length)
+	{
+		StringBase<char>::concat(text, length);
+	}
+};
 
 class File
 {
 public:
 	virtual int read(void *buffer, int bytes);
 	virtual void nextLine(char *buf, int bufSize);
+	virtual bool scanString(AsciiString &newString);
 
 protected:
 	void *m_nameStr;	// +0x04 AsciiString untouched here
@@ -29,6 +64,7 @@ class RAMFile : public File
 public:
 	virtual int read(void *buffer, int bytes);
 	virtual void nextLine(char *buf, int bufSize);
+	virtual bool scanString(AsciiString &newString);
 
 protected:
 	char *m_data;	// +0x14
@@ -94,4 +130,30 @@ void RAMFile::nextLine(char *buf, int bufSize)
 	if (m_pos >= m_size) {
 		m_pos = m_size;
 	}
+}
+
+// ?scanString@RAMFile@@UAE_NAAVAsciiString@@@Z @ 0x006056FA (128B): slot 9
+// (offset 0x24) of vtable 0x0087AA00. ZH GameEngine RAMFile::scanString
+// verbatim with BFME2 (temp,1) concat spelling like LocalFile::scanString.
+bool RAMFile::scanString(AsciiString &newString)
+{
+	newString.clear();
+
+	while ((m_pos < m_size) && isspace(m_data[m_pos])) {
+		++m_pos;
+	}
+
+	if (m_pos >= m_size) {
+		m_pos = m_size;
+		return false;
+	}
+
+	do {
+		char value[4];
+		value[0] = m_data[m_pos];
+		newString.concat(value, 1);
+		++m_pos;
+	} while ((m_pos < m_size) && (!isspace(m_data[m_pos])));
+
+	return true;
 }
