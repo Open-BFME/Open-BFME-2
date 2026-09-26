91c30ac5fb57f1344973dae10c3730b41ba5337a
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:22:52 +0000

    Recover SlaughterHordeContain::rva004625BB 53B @0x004625BB via slot-68 iterate wrapper

---
+++ Code/GameEngine/Source/GameLogic/Object/Contain/SlaughterHordeContainRva004625BB.cpp
@@ -0,0 +1,50 @@
+// cl: /O1 /DNDEBUG /MD
+//
+// ?rva004625BB@SlaughterHordeContain@@UAE_NPAXH@Z, retail 0x004625BB, 53 bytes.
+// Virtual slot 128 (offset 0x200) of vtable 0x00848AA0 (class of
+// ??0SlaughterHordeContain@@QAE@PAVThing@@PBVModuleData@@@Z in
+// SlaughterHordeContainCtor.cpp). Wrapper over slot 68 (offset 0x110)
+// iterateContained: builds a 9-byte user struct {first arg, second arg,
+// bool out}, passes the retail callback at 0x0046258C plus the struct plus
+// true to the slot-68 virtual, then returns out == false (xor, cmp, sete).
+// Evidence: vtable slots 128/129 share the same slot-68 caller shape (129 is
+// the void twin at 0x004625F0 pushing 0x004625AA), push-imm 0x86258c plus
+// call [eax+0x110] plus leave/ret-8 shape, no direct callees (DIR32-masked
+// callback, virtual iterate needs no row). Honest address name: class plus
+// slot are proven, method identity is not.
+
+class Object;
+typedef void (__cdecl *ContainIterateFunc)(Object *obj, void *userData);
+
+void iterCallback0046258C(Object *obj, void *userData);
+
+#define SLOT08(a,b,c,d,e,f,g,h) virtual void a(); virtual void b(); virtual void c(); virtual void d(); virtual void e(); virtual void f(); virtual void g(); virtual void h();
+#define SLOT16(a) SLOT08(a##0,a##1,a##2,a##3,a##4,a##5,a##6,a##7) SLOT08(a##8,a##9,a##A,a##B,a##C,a##D,a##E,a##F)
+
+class SlaughterHordeContain
+{
+public:
+	SLOT16(s0) SLOT16(s1) SLOT16(s2) SLOT16(s3)
+	virtual void s40(); virtual void s41(); virtual void s42(); virtual void s43();
+	virtual void iterateContained(ContainIterateFunc func, void *userData, bool reverse);
+	SLOT16(s69) SLOT16(s6A) SLOT16(s6B)
+	SLOT08(s6C0,s6C1,s6C2,s6C3,s6C4,s6C5,s6C6,s6C7)
+	virtual void s6D0(); virtual void s6D1(); virtual void s6D2();
+	virtual bool rva004625BB(void *a, int b);
+};
+
+struct IterateUser004625BB {
+	void *a;
+	int b;
+	bool flag;
+};
+
+bool SlaughterHordeContain::rva004625BB(void *a, int b)
+{
+	IterateUser004625BB user;
+	user.b = b;
+	user.a = a;
+	user.flag = false;
+	iterateContained(iterCallback0046258C, &user, true);
+	return user.flag == false;
+}
