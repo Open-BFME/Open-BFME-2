624f9e980430feca7192fb26404a88d1f3dfc04a
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 13:43:01 +0000

    Recover SpawnBehaviorModuleData deleting dtor 28B @0x00257653 via FamilyDeletingDtors pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-2335.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-2335.cpp
@@ -5,3 +5,9 @@ class RandomSoundSelectorClientBehaviorModuleData { public: __declspec(noinline)
 // ??1RandomSoundSelectorClientBehaviorModuleData@@UAE@XZ present-unmatched
 RandomSoundSelectorClientBehaviorModuleData::~RandomSoundSelectorClientBehaviorModuleData() { m_famgen = 0; }
 void famgenDelete(RandomSoundSelectorClientBehaviorModuleData *p) { delete p; }
+// ??_GSpawnBehaviorModuleData@@UAEPAXI@Z @0x00257653 28B
+// Deleting dtor slot 0 of vtable 0x007F4128; calls rowed ??1 at 0x0025766F then rowed operator delete at 0x0002FD60.
+class SpawnBehaviorModuleData { public: __declspec(noinline) virtual ~SpawnBehaviorModuleData(); private: int m_famgen; };
+// ??1SpawnBehaviorModuleData@@UAE@XZ present-unmatched
+SpawnBehaviorModuleData::~SpawnBehaviorModuleData() { m_famgen = 0; }
+void famgenDelete(SpawnBehaviorModuleData *p) { delete p; }
