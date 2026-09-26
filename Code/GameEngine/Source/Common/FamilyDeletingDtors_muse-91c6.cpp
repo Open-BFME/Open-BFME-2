9b6d335250236e7662ca033d4d9d14a9b8e1e6f6
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:07:08 +0000

    Recover AutoDepositUpdateModuleData deleting dtor 28B @0x0025528B via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-91c6.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-91c6.cpp
@@ -4,3 +4,8 @@
 class RunOffMapBehaviorModuleData { public: __declspec(noinline) virtual ~RunOffMapBehaviorModuleData(); private: int m_famgen; };
 RunOffMapBehaviorModuleData::~RunOffMapBehaviorModuleData() { m_famgen = 0; }
 void famgenDelete(RunOffMapBehaviorModuleData *p) { delete p; }
+// ??_GAutoDepositUpdateModuleData@@UAEPAXI@Z @0x0025528B 28B
+// Deleting dtor slot 0 of vtable 0x007F1BC8; calls rowed ??1AutoDepositUpdateModuleData@@UAE@XZ at 0x002552A7 then rowed operator delete at 0x0002FD60.
+class AutoDepositUpdateModuleData { public: __declspec(noinline) virtual ~AutoDepositUpdateModuleData(); private: int m_famgen; };
+AutoDepositUpdateModuleData::~AutoDepositUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(AutoDepositUpdateModuleData *p) { delete p; }
