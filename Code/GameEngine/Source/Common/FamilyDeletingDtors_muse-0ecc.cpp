9ad0977573660386cf78cfb4bc6850be3598917f
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:33:56 +0000

    Recover BuildingBehaviorModuleData deleting dtor 28B @0x004564CA via family ??_G pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-0ecc.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-0ecc.cpp
@@ -8,3 +8,13 @@ class AttributeModifierAuraUpdateModuleData { public: __declspec(noinline) virtu
   friend void famgenDelete(AttributeModifierAuraUpdateModuleData *p); };
 AttributeModifierAuraUpdateModuleData::~AttributeModifierAuraUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(AttributeModifierAuraUpdateModuleData *p) { delete p; }
+
+// ??_GBuildingBehaviorModuleData@@UAEPAXI@Z @0x004564CA, 28B.
+// Scalar deleting dtor slot 0 of vtable 0x00840798; calls rowed ??1 at
+// 0x004564E6 plus rowed delete at 0x0002FD60.
+
+// ??_GBuildingBehaviorModuleData@@UAEPAXI@Z @0x004564CA
+class BuildingBehaviorModuleData { public: __declspec(noinline) virtual ~BuildingBehaviorModuleData(); private: int m_famgen;
+  friend void famgenDelete(BuildingBehaviorModuleData *p); };
+BuildingBehaviorModuleData::~BuildingBehaviorModuleData() { m_famgen = 0; }
+void famgenDelete(BuildingBehaviorModuleData *p) { delete p; }
