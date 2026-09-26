787b88629a1bf7f3bf6d5ba2cbf21bb8ea9a2efb
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 13:39:14 +0000

    Recover AIUpdateModuleData deleting dtor 28B @0x0058957E via famgen pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-5c79.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-5c79.cpp
@@ -4,3 +4,9 @@
 class SiegeDeployHordeSpecialPower { public: __declspec(noinline) virtual ~SiegeDeployHordeSpecialPower(); private: int m_famgen; };
 SiegeDeployHordeSpecialPower::~SiegeDeployHordeSpecialPower() { m_famgen = 0; }
 void famgenDelete(SiegeDeployHordeSpecialPower *p) { delete p; }
+
+// ??_GAIUpdateModuleData@@UAEPAXI@Z @0x0058957E 28B
+// Deleting dtor slot 0 of vtable 0x00870220; calls rowed ??1 at 0x00494BE4 then rowed operator delete at 0x0002FD60.
+class AIUpdateModuleData { public: __declspec(noinline) virtual ~AIUpdateModuleData(); private: int m_famgen; };
+AIUpdateModuleData::~AIUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(AIUpdateModuleData *p) { delete p; }
