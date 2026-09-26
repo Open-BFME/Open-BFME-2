8b68aebee98c9ab8cb8300460412f5fccfccd784
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:10:20 +0000

    Recover OneRingPenaltyUpdate::xfer 82B @0x0049995A via slot-3 xfer pattern

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/OneRingPenaltyUpdateXfer.cpp
@@ -0,0 +1,151 @@
+// cl: /O1 /DNDEBUG /MD
+
+// ?xfer@OneRingPenaltyUpdate@@MAEXPAVXfer@@@Z, retail 0x0049995A, 82 bytes.
+// Slot 3 (offset 0x0C) of vtable 0x00850260 installed by the rowed ctor
+// 0x00499A70 (news 0x34). Base UpdateModule xfer via rowed 0x0044DF9F then
+// IsLightCRC early-out via Xfer slot 0x10 then Version1 via rowed 0x000053EE
+// then ObjectID at +0x24 via rowed XferObjectID 0x003060B2 plus uint at +0x28
+// via Xfer slot 0x78 plus float at +0x2C via Xfer slot 0x70 plus uint at +0x30
+// via Xfer slot 0x78. Layout is the rowed 0x34-byte class from
+// OneRingPenaltyUpdateCtor.cpp (UpdateModule base 0x20 plus trailing +0x20
+// plus ObjectID uint float uint). Recipe is the DestroyEnvironmentUpdateXfer
+// slot-3 pattern.
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+class Coord3DBase;
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+enum ObjectID
+{
+	INVALID_ID = 0
+};
+
+void XferObjectID(Xfer *xfer, ObjectID *objectID);
+
+class ObjectModule
+{
+public:
+	ObjectModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~ObjectModule();
+
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+
+class BehaviorModuleInterface { public: virtual void behaviorSlot(); };
+class UpdateModuleInterface { public: virtual void updateSlot(); };
+
+class UpdateModule : public ObjectModule,
+	public BehaviorModuleInterface, public UpdateModuleInterface
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~UpdateModule();
+	void xfer(Xfer *xfer);
+
+private:
+	unsigned int m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	int m_updateState;
+};
+
+class OneRingPenaltyUpdate : public UpdateModule
+{
+public:
+	OneRingPenaltyUpdate(Thing *thing, const ModuleData *moduleData);
+
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	const void *m_20;
+	ObjectID m_objectID;
+	unsigned int m_28;
+	float m_2C;
+	unsigned int m_30;
+};
+
+void OneRingPenaltyUpdate::xfer(Xfer *xfer)
+{
+	UpdateModule::xfer(xfer);
+	if (xfer->IsLightCRC())
+		return;
+	xfer->Version1();
+	XferObjectID(xfer, &m_objectID);
+	*xfer == m_28;
+	*xfer == m_2C;
+	*xfer == m_30;
+}
