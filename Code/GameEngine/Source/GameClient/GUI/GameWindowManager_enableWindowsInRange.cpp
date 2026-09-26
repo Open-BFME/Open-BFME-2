42b4965d1f426fd99618c2a0ad39a03aea767ff7
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:52:24 +0000

    Recover GameWindowManager::enableWindowsInRange 52B @0x002C0DD0 via BFME1 donor verbatim

---
+++ Code/GameEngine/Source/GameClient/GUI/GameWindowManager_enableWindowsInRange.cpp
@@ -0,0 +1,44 @@
+// cl: /O1 /DNDEBUG /MD
+// ?enableWindowsInRange@GameWindowManager@@UAEXPAVGameWindow@@HH_N@Z @0x002C0DD0 52B
+// GameWindowManager::enableWindowsInRange; BFME1 donor GameWindowManager.cpp enableWindowsInRange verbatim shape; vtable 0x7C7C90 slot 39; calls winGetWindowFromId slot 60 0xF0 and rowed winEnable 0x313BEC.
+
+typedef int Int;
+typedef bool Bool;
+
+class GameWindow
+{
+public:
+	Int winEnable(Bool enable);
+};
+
+class GameWindowManager
+{
+public:
+#define V(n) virtual void pad##n() = 0;
+	V(0) V(1) V(2) V(3) V(4) V(5) V(6) V(7)
+	V(8) V(9) V(10) V(11) V(12) V(13) V(14) V(15)
+	V(16) V(17) V(18) V(19) V(20) V(21) V(22) V(23)
+	V(24) V(25) V(26) V(27) V(28) V(29) V(30) V(31)
+	V(32) V(33) V(34) V(35) V(36) V(37) V(38)
+#undef V
+	virtual void enableWindowsInRange(GameWindow *baseWindow, Int first, Int last, Bool enableFlag);
+#define W(n) virtual void pad##n() = 0;
+	W(40) W(41) W(42) W(43) W(44) W(45) W(46) W(47)
+	W(48) W(49) W(50) W(51) W(52) W(53) W(54) W(55)
+	W(56) W(57) W(58) W(59)
+#undef W
+	virtual GameWindow *winGetWindowFromId(GameWindow *window, Int id) = 0;
+};
+
+void GameWindowManager::enableWindowsInRange(GameWindow *baseWindow, Int first, Int last, Bool enableFlag)
+{
+	Int i;
+	GameWindow *window;
+
+	for (i = first; i <= last; i++)
+	{
+		window = winGetWindowFromId(baseWindow, i);
+		if (window)
+			window->winEnable(enableFlag);
+	}
+}
