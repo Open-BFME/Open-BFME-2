61821d8c1e21165c5034b2e4bfd604353d9c951f
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 09:00:38 +0000

    Recover DelayedWeaponSetUpgradeUpdate deleting dtor 28B @0x0048831C via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-a8e6.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-a8e6.cpp
@@ -3,3 +3,9 @@
 class W3DStreakDrawModuleData { public: __declspec(noinline) virtual ~W3DStreakDrawModuleData(); private: int m_famgen; };
 W3DStreakDrawModuleData::~W3DStreakDrawModuleData() { m_famgen = 0; }
 void famgenDelete(W3DStreakDrawModuleData *p) { delete p; }
+
+// ??_GDelayedWeaponSetUpgradeUpdate@@MAEPAXI@Z @0x0048831C
+class DelayedWeaponSetUpgradeUpdate { protected: __declspec(noinline) virtual ~DelayedWeaponSetUpgradeUpdate(); private: int m_famgen;
+  friend void famgenDelete(DelayedWeaponSetUpgradeUpdate *p); };
+DelayedWeaponSetUpgradeUpdate::~DelayedWeaponSetUpgradeUpdate() { m_famgen = 0; }
+void famgenDelete(DelayedWeaponSetUpgradeUpdate *p) { delete p; }
