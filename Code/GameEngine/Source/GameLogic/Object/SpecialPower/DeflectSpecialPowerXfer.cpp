ba84658c24781b06648410f71ce462681f9585b8
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:27:32 +0000

    Recover DeflectSpecialPower::xfer 65B @0x004C5571 via IsLightCRC Version1 bools

---
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/DeflectSpecialPowerXfer.cpp
@@ -0,0 +1,166 @@
+// cl: /O1 /DNDEBUG /MD
+//
+// ?xfer@DeflectSpecialPower@@MAEXPAVXfer@@@Z, retail 0x004C5571, 65 bytes.
+// Virtual slot 3 (offset 0x0C) of vtable 0x0085DA7C (class of rowed ctor
+// ??0DeflectSpecialPower@@QAE@PAVThing@@PBVModuleData@@@Z in
+// DeflectSpecialPowerConstructorThunk.cpp). Base Rva00589079 xfer via rowed
+// 0x00589215 then IsLightCRC early-out via Xfer slot 0x10 then Version1 via
+// rowed 0x000053EE then bytes at +0x3C/+0x3D via Xfer slot 0x90. Layout is
+// Rva00589079 base 0x38 plus pad to +0x3C plus two bytes (cf. rowed ctor
+// zeroes m_byte3C/m_byte3D). Donor pattern is SiegeDeployHordeSpecialPowerXfer
+// (IsLightCRC plus Version plus gated tail).
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+struct Coord3DBase
+{
+	float x;
+	float y;
+	float z;
+};
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+class ObjectModule
+{
+public:
+	ObjectModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~ObjectModule();
+
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+
+class BehaviorModuleInterface { public: virtual void behaviorSlot(); };
+class UpdateModuleInterface { public: virtual void updateSlot(); };
+
+class UpdateModule : public ObjectModule,
+	public BehaviorModuleInterface, public UpdateModuleInterface
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~UpdateModule();
+	void xfer(Xfer *xfer);
+
+private:
+	unsigned int m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	int m_updateState;
+};
+
+class Rva00589079_E1
+{
+public:
+	virtual void fe();
+};
+
+class Rva00589079_E2
+{
+public:
+	virtual void fe();
+};
+
+class Rva00589079 : public UpdateModule, public Rva00589079_E1, public Rva00589079_E2
+{
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	unsigned int m_28;
+	int m_2C;
+	unsigned int m_30;
+	float m_34;
+};
+
+class DeflectSpecialPower : public Rva00589079
+{
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	unsigned int m_pad38;
+	bool m_byte3C;
+	bool m_byte3D;
+	char m_pad3E[2];
+};
+
+void DeflectSpecialPower::xfer(Xfer *xfer)
+{
+	Rva00589079::xfer(xfer);
+	if (xfer->IsLightCRC())
+		return;
+	xfer->Version1();
+	*xfer == m_byte3C;
+	*xfer == m_byte3D;
+}
