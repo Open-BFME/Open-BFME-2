e532d6cc0911fdc9cbb3a0509e6b3696d55231be
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:18:03 +0000

    Recover GrantUpgradeCreateModuleData deleting dtor 28B @0x004B912F via family shard pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-4f8a.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-4f8a.cpp
@@ -4,3 +4,9 @@
 class RebuildHoleExposeDieModuleData { public: __declspec(noinline) virtual ~RebuildHoleExposeDieModuleData(); private: int m_famgen; };
 RebuildHoleExposeDieModuleData::~RebuildHoleExposeDieModuleData() { m_famgen = 0; }
 void famgenDelete(RebuildHoleExposeDieModuleData *p) { delete p; }
+
+// ??_GGrantUpgradeCreateModuleData@@UAEPAXI@Z @0x004B912F
+// Deleting dtor slot 0 of vtable 0xC594E0; calls the rowed ??1 at 0x4B914B.
+class GrantUpgradeCreateModuleData { public: __declspec(noinline) virtual ~GrantUpgradeCreateModuleData(); private: int m_famgen; };
+GrantUpgradeCreateModuleData::~GrantUpgradeCreateModuleData() { m_famgen = 0; }
+void famgenDelete(GrantUpgradeCreateModuleData *p) { delete p; }
