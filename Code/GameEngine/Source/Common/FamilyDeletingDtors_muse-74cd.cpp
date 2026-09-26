d69e01ae8dd00c9d37e1baf00b417efbe3968ba1
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 13:32:04 +0000

    Recover Gen0014AE40 deleting dtor 28B @0x002FE6F6 via slot0 rowed dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-74cd.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-74cd.cpp
@@ -5,3 +5,8 @@ class DynamicPortalBehaviour { protected: __declspec(noinline) virtual ~DynamicP
   friend void famgenDelete(DynamicPortalBehaviour *p); };
 DynamicPortalBehaviour::~DynamicPortalBehaviour() { m_famgen = 0; }
 void famgenDelete(DynamicPortalBehaviour *p) { delete p; }
+// ??_GGen0014AE40@@UAEPAXI@Z @0x002FE6F6 28B
+// Deleting dtor slot 0 of vtable 0x008071FC; calls rowed ??1 at 0x002FE0A5 then rowed operator delete at 0x0002FD60.
+class Gen0014AE40 { public: __declspec(noinline) virtual ~Gen0014AE40(); private: int m_famgen; };
+Gen0014AE40::~Gen0014AE40() { m_famgen = 0; }
+void famgenDeleteGen(Gen0014AE40 *p) { delete p; }
