c778c3f1232883429278498036b9b91bb34ee6be
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:08:16 +0000

    Recover W3DFloorDrawModuleData deleting dtor 28B @0x000CF820 via family slot0 pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-e4e5.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-e4e5.cpp
@@ -4,3 +4,9 @@
 class W3DPropDrawModuleData { public: __declspec(noinline) virtual ~W3DPropDrawModuleData(); private: int m_famgen; };
 W3DPropDrawModuleData::~W3DPropDrawModuleData() { m_famgen = 0; }
 void famgenDelete(W3DPropDrawModuleData *p) { delete p; }
+
+// ??_GW3DFloorDrawModuleData@@UAEPAXI@Z @0x000CF820 28B
+// Deleting dtor slot 0 of vtable 0x00BCD5D8; calls rowed ??1W3DFloorDrawModuleData@@UAE@XZ at 0x000CF6C3 then rowed operator delete at 0x0002FD60.
+class W3DFloorDrawModuleData { public: __declspec(noinline) virtual ~W3DFloorDrawModuleData(); private: int m_famgen; };
+W3DFloorDrawModuleData::~W3DFloorDrawModuleData() { m_famgen = 0; }
+void famgenDelete(W3DFloorDrawModuleData *p) { delete p; }
