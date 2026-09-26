08ef0e03e7dc35689b7cabd34e04b3928e5ef3f5
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:56:06 +0000

    Recover ScriptActions deleting dtor 28B @0x003BE5AD via slot0 deleting dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-1fea.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-1fea.cpp
@@ -4,3 +4,8 @@
 class LevelGrantSpecialPowerModuleData { public: __declspec(noinline) virtual ~LevelGrantSpecialPowerModuleData(); private: int m_famgen; };
 LevelGrantSpecialPowerModuleData::~LevelGrantSpecialPowerModuleData() { m_famgen = 0; }
 void famgenDelete(LevelGrantSpecialPowerModuleData *p) { delete p; }
+// ??_GScriptActions@@UAEPAXI@Z @0x003BE5AD 28B
+// Deleting dtor slot 0 of vtable 0x0081FE48; calls rowed ??1 at 0x003BD582 then rowed operator delete at 0x0002FD60.
+class ScriptActions { public: __declspec(noinline) virtual ~ScriptActions(); private: int m_famgen; };
+ScriptActions::~ScriptActions() { m_famgen = 0; }
+void famgenDelete(ScriptActions *p) { delete p; }
