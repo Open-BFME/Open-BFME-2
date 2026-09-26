5e76cc4881911cd8194287ef31c22f21208ee6fc
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 09:07:23 +0000

    Recover PillageModuleData deleting dtor 28B @0x00254FFF via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-e2de.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-e2de.cpp
@@ -4,3 +4,9 @@
 class SpecialEnemySenseUpdateModuleData { public: __declspec(noinline) virtual ~SpecialEnemySenseUpdateModuleData(); private: int m_famgen; };
 SpecialEnemySenseUpdateModuleData::~SpecialEnemySenseUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(SpecialEnemySenseUpdateModuleData *p) { delete p; }
+
+// ??_GPillageModuleData@@UAEPAXI@Z @0x00254FFF
+// Deleting dtor slot 0 of vtable 0xBF0568; calls the rowed ??1 at 0x25501B.
+class PillageModuleData { public: __declspec(noinline) virtual ~PillageModuleData(); private: int m_famgen; };
+PillageModuleData::~PillageModuleData() { m_famgen = 0; }
+void famgenDelete(PillageModuleData *p) { delete p; }
