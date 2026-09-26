c65dfc8a8867824e2b31214335bb958732c9b0c1
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:34:23 +0000

    Recover Rva00491DD3 xfer 66B @0x00491F18 via base Version bool uint

---
+++ Code/GameEngine/Source/Common/Rva00491DD3Xfer.cpp
@@ -0,0 +1,150 @@
+// cl: /O1 /DNDEBUG /MD
+//
+// ?xfer@Rva00491DD3@@MAEXPAVXfer@@@Z, retail 0x00491F18, 66 bytes. Virtual
+// slot 3 (offset 0x0C) of vtable 0x0084DDA8 (class of rowed dtor
+// ??1Rva00491DD3@@UAE@XZ in Rva0024A797Derived.cpp): base UpdateModule xfer
+// via rowed 0x44DF9F first, then Version(1,1) via Xfer slot 0x28 reusing the
+// dead argument slot, then bool at +0x20 via Xfer slot 0x90, then uint at
+// +0x24 via Xfer slot 0x78. Layout is UpdateModule base 0x20 plus bool plus
+// uint. Identity is slot 3 plus the UpdateModule base call; class name
+// stays honest Rva address name.
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+class Coord3DBase;
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+class Xfer::Version
+{
+public:
+	Version(unsigned char current, unsigned char minimum)
+		: m_current(current), m_minimum(minimum) {}
+
+	unsigned char m_current;
+	unsigned char m_minimum;
+};
+
+class BehaviorModuleBase
+{
+	virtual void unused();
+	int a;
+	int b;
+};
+
+class BehaviorModuleOther
+{
+	virtual void unused();
+};
+
+class BehaviorModule : public BehaviorModuleBase, public BehaviorModuleOther
+{
+public:
+	BehaviorModule(Thing *thing, const ModuleData *moduleData);
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void update() = 0;
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+	unsigned m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	int m_bfmeReserved;
+
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData);
+	void xfer(Xfer *xfer);
+};
+
+class Rva00491DD3 : public UpdateModule
+{
+protected:
+	virtual void xfer(Xfer *xfer);
+
+private:
+	bool m_20; // +0x20
+	unsigned int m_24; // +0x24
+};
+
+void Rva00491DD3::xfer(Xfer *xfer)
+{
+	UpdateModule::xfer(xfer);
+	Xfer::Version version(1, 1);
+	*xfer == version;
+	*xfer == m_20;
+	*xfer == m_24;
+}
