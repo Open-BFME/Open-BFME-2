84fe717482f1de30a12cfefa3f8714e6ea73a9ae
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:13:24 +0000

    Recover GiveUpgradeUpdateModuleData deleting dtor 28B @0x0049C624 via family ??_G pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-7f75.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-7f75.cpp
@@ -4,3 +4,9 @@
 class RadiateFearUpdateModuleData { public: __declspec(noinline) virtual ~RadiateFearUpdateModuleData(); private: int m_famgen; };
 RadiateFearUpdateModuleData::~RadiateFearUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(RadiateFearUpdateModuleData *p) { delete p; }
+
+// ??_GGiveUpgradeUpdateModuleData@@UAEPAXI@Z @0x0049C624 28B
+// Deleting dtor slot 0 of vtable 0x00851280; calls rowed ??1 at 0x0049C640 then rowed operator delete at 0x0002FD60.
+class GiveUpgradeUpdateModuleData { public: __declspec(noinline) virtual ~GiveUpgradeUpdateModuleData(); private: int m_famgen; };
+GiveUpgradeUpdateModuleData::~GiveUpgradeUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(GiveUpgradeUpdateModuleData *p) { delete p; }
