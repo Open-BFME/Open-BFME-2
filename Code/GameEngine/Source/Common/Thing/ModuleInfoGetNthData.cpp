93909e84314a3f547da8a5b6c311b4c733abad14
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 01:57:31 +0000

    Recover ThingTemplate friend_getAIModuleInfo 80B @0x0033B387 via triple getNthData donor shape

--- Code/GameEngine/Source/Common/Thing/ModuleInfoGetNthData.cpp
+++ Code/GameEngine/Source/Common/Thing/ModuleInfoGetNthData.cpp
@@ -1,7 +1,17 @@
 // cl: /O1 /G7 /MD /DNDEBUG
 // ModuleInfo indexed accessor (Locomotor/AIUpdate parse cluster).
 
-class ModuleData;
+class ModuleData
+{
+public:
+	virtual void bfmeReservedV0();
+	virtual void bfmeReservedV1();
+	virtual void bfmeReservedV2();
+	virtual void bfmeReservedV3();
+	virtual bool isAiModuleData() const;
+};
+
+class AIUpdateModuleData;
 
 struct BfmeModuleNugget
 {
@@ -16,9 +26,23 @@ class ModuleInfo
 {
 	const void *m_begin;
 	const void *m_end;
+	const void *m_storage;
 
 public:
-	const ModuleData *getNthData(int i) const;
+	int getCount() const
+	{
+		return ((const char *)m_end - (const char *)m_begin) / 20;
+	}
+	__declspec(noinline) const ModuleData *getNthData(int i) const;
+};
+
+class ThingTemplate
+{
+	char m_pad[0x2E4];
+	ModuleInfo m_behaviorModuleInfo;
+
+public:
+	AIUpdateModuleData *friend_getAIModuleInfo();
 };
 
 const ModuleData *ModuleInfo::getNthData(int i) const
@@ -30,6 +54,17 @@ const ModuleData *ModuleInfo::getNthData(int i) const
 		unsigned int n = (e - b) / 20;
 		if (i < n)
 			return *(const ModuleData * const *)(b + i * 20 + 8);
+	}
+	return 0;
+}
+
+AIUpdateModuleData *ThingTemplate::friend_getAIModuleInfo()
+{
+	int numModInfos = m_behaviorModuleInfo.getCount();
+	for (int j = 0; j < numModInfos; ++j)
+	{
+		if (m_behaviorModuleInfo.getNthData(j) && m_behaviorModuleInfo.getNthData(j)->isAiModuleData())
+			return (AIUpdateModuleData *)m_behaviorModuleInfo.getNthData(j);
 	}
 	return 0;
 }
