3fa7353e625b9fc310a3b8a88c9bb72eafc2121a
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:34:32 +0000

    Recover InheritUpgradeCreate::xfer 37B @0x004B94E3 via base plus Version 1 1

---
+++ Code/GameEngine/Source/GameLogic/Object/Create/InheritUpgradeCreateXfer.cpp
@@ -0,0 +1,119 @@
+// cl: /O1 /DNDEBUG /MD
+//
+// ?xfer@InheritUpgradeCreate@@MAEXPAVXfer@@@Z, retail 0x004B94E3, 37 bytes.
+// Slot 3 (offset 0x0C) of vtable 0x008596F4 (class of rowed ctor
+// ??0InheritUpgradeCreate@@QAE@PAVThing@@PBVModuleData@@@Z in
+// InheritUpgradeCreateCtor.cpp): base CreateModule xfer via rowed 0x4B8D4E
+// then Version(1,1) via Xfer slot 0x28. Recipe is the InvisibilityUpdateXfer
+// slot-3 pattern with base first then Version.
+
+class AsciiString;
+class UnicodeString;
+class PooledString;
+struct XferUnknown11;
+class Coord3DBase;
+class ICoord3D;
+class Region3D;
+class IRegion3D;
+class Coord2D;
+class ICoord2D;
+class Region2D;
+class IRegion2D;
+class RealRange;
+class RGBColor;
+class RGBAColorReal;
+class RGBAColorInt;
+class Snapshot;
+class Thing;
+class ModuleData;
+class Object;
+
+class Xfer
+{
+public:
+	class Version;
+
+	Xfer();
+	virtual ~Xfer();
+
+	void Version1();
+
+	virtual bool IsLoading() const;
+	virtual bool IsStoring() const;
+	virtual bool IsCRC() const;
+	virtual bool IsLightCRC() const;
+
+	virtual void v5() = 0;
+	virtual void v6() = 0;
+	virtual void v7() = 0;
+
+	virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
+	virtual Xfer &XferRawBytes(void *data, unsigned int size);
+	virtual Xfer &operator==(bool &value);
+	virtual Xfer &operator==(char &value);
+	virtual Xfer &operator==(unsigned char &value);
+	virtual Xfer &operator==(short &value);
+	virtual Xfer &operator==(unsigned short &value);
+	virtual Xfer &operator==(int &value);
+	virtual Xfer &operator==(unsigned int &value);
+	virtual Xfer &operator==(__int64 &value);
+	virtual Xfer &operator==(float &value);
+	virtual Xfer &operator==(AsciiString &value);
+	virtual Xfer &operator==(UnicodeString &value);
+	virtual Xfer &operator==(PooledString &value);
+	virtual Xfer &operator==(Coord3DBase &value);
+	virtual Xfer &operator==(ICoord3D &value);
+	virtual Xfer &operator==(Region3D &value);
+	virtual Xfer &operator==(IRegion3D &value);
+	virtual Xfer &operator==(Coord2D &value);
+	virtual Xfer &operator==(ICoord2D &value);
+	virtual Xfer &operator==(Region2D &value);
+	virtual Xfer &operator==(IRegion2D &value);
+	virtual Xfer &operator==(RealRange &value);
+	virtual Xfer &operator==(RGBColor &value);
+	virtual Xfer &operator==(RGBAColorReal &value);
+	virtual Xfer &operator==(RGBAColorInt &value);
+	virtual Xfer &operator==(Snapshot &value);
+	virtual Xfer &operator==(XferUnknown11 &value) = 0;
+	virtual Xfer &operator==(Version &value);
+
+	virtual Xfer &XferEnum(const char *name, void *data, unsigned int size);
+
+protected:
+	virtual void XferData(unsigned int type, void *data, unsigned int size) = 0;
+};
+
+class Xfer::Version
+{
+public:
+	Version(unsigned char current, unsigned char minimum)
+		: m_current(current), m_minimum(minimum) {}
+
+	unsigned char m_current;
+	unsigned char m_minimum;
+};
+
+class CreateModule
+{
+public:
+	CreateModule(Thing *thing, const ModuleData *moduleData);
+
+protected:
+	virtual void xfer(Xfer *xfer);
+};
+
+class InheritUpgradeCreate : public CreateModule
+{
+public:
+	InheritUpgradeCreate(Thing *thing, const ModuleData *moduleData);
+
+protected:
+	virtual void xfer(Xfer *xfer);
+};
+
+void InheritUpgradeCreate::xfer(Xfer *xfer)
+{
+	CreateModule::xfer(xfer);
+	Xfer::Version version(1, 1);
+	*xfer == version;
+}
