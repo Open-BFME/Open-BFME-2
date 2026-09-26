14ded83a8c6b68c7fff310a0b61654d582941a9a
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:23:56 +0000

    Recover StrafeAreaUpdateModuleData deleting dtor 28B @0x003A4F05 via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-eeab.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-eeab.cpp
@@ -3,3 +3,7 @@
 class CivilianSpawnUpdateModuleData { public: __declspec(noinline) virtual ~CivilianSpawnUpdateModuleData(); private: int m_famgen; };
 CivilianSpawnUpdateModuleData::~CivilianSpawnUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(CivilianSpawnUpdateModuleData *p) { delete p; }
+// ??_GStrafeAreaUpdateModuleData@@UAEPAXI@Z @0x003A4F05
+class StrafeAreaUpdateModuleData { public: __declspec(noinline) virtual ~StrafeAreaUpdateModuleData(); private: int m_famgen; };
+StrafeAreaUpdateModuleData::~StrafeAreaUpdateModuleData() { m_famgen = 0; }
+void famgenDelete(StrafeAreaUpdateModuleData *p) { delete p; }
