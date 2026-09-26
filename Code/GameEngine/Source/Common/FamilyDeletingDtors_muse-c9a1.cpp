77c8253c5a3e49a97213dfb1a40274721fa9999f
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 13:26:37 +0000

    Recover BuffNuggetFXNugget deleting dtor 28B @0x001E32B7 via famgen pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-c9a1.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-c9a1.cpp
@@ -10,3 +10,9 @@ void famgenDelete(FXListAtBonePosFXNugget *p) { delete p; }
 class ParticleSystemFXNugget { public: __declspec(noinline) virtual ~ParticleSystemFXNugget(); private: int m_famgen; };
 ParticleSystemFXNugget::~ParticleSystemFXNugget() { m_famgen = 0; }
 void famgenDelete(ParticleSystemFXNugget *p) { delete p; }
+
+// ??_GBuffNuggetFXNugget@@UAEPAXI@Z @0x001E32B7 28B
+// Deleting dtor slot 0 of vtable 0x007DD9B4; calls rowed ??1 at 0x001E32D3 then rowed operator delete at 0x0002FD60.
+class BuffNuggetFXNugget { public: __declspec(noinline) virtual ~BuffNuggetFXNugget(); private: int m_famgen; };
+BuffNuggetFXNugget::~BuffNuggetFXNugget() { m_famgen = 0; }
+void famgenDelete(BuffNuggetFXNugget *p) { delete p; }
