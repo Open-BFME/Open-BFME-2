381272790163c9c86fed1936f7098a21fdee4765
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 16:04:11 +0000

    Recover Rva0035DCF9::Rva0035DD08 148B @0x0035DD08 via vslot range-select hide

--- Code/GameEngine/Source/Common/FamilyTailDtors1DBAC3.cpp
+++ Code/GameEngine/Source/Common/FamilyTailDtors1DBAC3.cpp
@@ -6,6 +6,9 @@
 // vslot 0x0035DD9C (?Rva0035DD9C@Rva0035DCF9@@UAEXXZ, 33B, slot 3 offset 0xC
 // of vtable 0x00816574): hide windows at +0xC and +0x44 via rowed winHide
 // 0x00313C64; clears +0x8/+0x9. Evidence: vtable 0x00816574.
+// vslot 0x0035DD08 (?Rva0035DD08@Rva0035DCF9@@UAEXH@Z, 148B, slot 2 offset 0x8
+// of vtable 0x00816574): range-select hide via rowed winHide 0x00313C64;
+// sets +0x28 to -1 then to index in middle range. Evidence: vtable 0x00816574.
 
 class Rva001DBAC3Base { public: virtual ~Rva001DBAC3Base(); };
 
@@ -32,7 +35,7 @@ class Rva0035DCF9 : public Rva001DBAC3Base
 public:
 	virtual ~Rva0035DCF9();
 	virtual void slot1();
-	virtual void slot2();
+	virtual void Rva0035DD08(int index);
 	virtual void Rva0035DD9C();
 	virtual void slot4();
 	virtual void slot5();
@@ -44,7 +47,11 @@ private:
 	bool m_9;
 	char m_padA[2];
 	GameWindow *m_C;
-	char m_pad10[0x44 - 0x10];
+	int m_10;
+	int m_14;
+	char m_pad18[0x28 - 0x18];
+	int m_28;
+	char m_pad2C[0x44 - 0x2C];
 	GameWindow *m_44;
 };
 Rva0035DCF9::~Rva0035DCF9() { m_C = 0; }
@@ -57,6 +64,37 @@ void Rva0035DCF9::Rva0035DD9C()
 	m_9 = false;
 	w1->winHide(true);
 	m_44->winHide(true);
+}
+
+void Rva0035DCF9::Rva0035DD08(int index)
+{
+	m_28 = -1;
+	if (index < m_10)
+		return;
+	if (index > m_14)
+		return;
+	if (index == m_10) {
+		if (!m_9 && m_C != 0 && m_44 != 0) {
+			m_C->winHide(false);
+			m_44->winHide(true);
+			m_8 = true;
+		}
+	} else if (index == m_14) {
+		if (m_9 && m_C != 0 && m_44 != 0) {
+			m_C->winHide(true);
+			m_44->winHide(false);
+			m_8 = true;
+		}
+	}
+	if (index <= m_10)
+		return;
+	if (index >= m_14)
+		return;
+	if (m_C != 0)
+		m_C->winHide(true);
+	if (m_44 != 0)
+		m_44->winHide(true);
+	m_28 = index;
 }
 
 class Rva0035E00F : public Rva001DBAC3Base { public: virtual ~Rva0035E00F(); int m_pad[2]; int m_field; };
