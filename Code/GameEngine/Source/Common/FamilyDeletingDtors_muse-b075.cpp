4354433bb27050168f6a14ed35053898f9ada432
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 12:31:38 +0000

    Recover DelayedLuaEventUpdate deleting dtor 28B @0x004A8EEF via family pattern

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-b075.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-b075.cpp
@@ -4,3 +4,9 @@
 class ToppleUpdateModuleData { public: __declspec(noinline) virtual ~ToppleUpdateModuleData(); private: int m_famgen; };
 ToppleUpdateModuleData::~ToppleUpdateModuleData() { m_famgen = 0; }
 void famgenDelete(ToppleUpdateModuleData *p) { delete p; }
+
+// ??_GDelayedLuaEventUpdate@@UAEPAXI@Z @0x004A8EEF 28B
+// Deleting dtor slot 0 of vtable 0x00853A5C; calls rowed ??1 at 0x004A8D4A then rowed operator delete at 0x0002FD60.
+class DelayedLuaEventUpdate { public: __declspec(noinline) virtual ~DelayedLuaEventUpdate(); private: int m_famgen; };
+DelayedLuaEventUpdate::~DelayedLuaEventUpdate() { m_famgen = 0; }
+void famgenDelete(DelayedLuaEventUpdate *p) { delete p; }
