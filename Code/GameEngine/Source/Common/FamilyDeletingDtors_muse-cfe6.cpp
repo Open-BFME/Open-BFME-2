2a1d9283a57d956a903f4a97949c2e18644586d9
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 15:39:44 +0000

    Recover CommandSetUpgradeModuleData deleting dtor 28B @0x00256267 via slot0 rowed dtor

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-cfe6.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-cfe6.cpp
@@ -13,3 +13,11 @@ class SpawnUnitBehaviorModuleData { public: __declspec(noinline) virtual ~SpawnU
 // ??1SpawnUnitBehaviorModuleData@@UAE@XZ present-unmatched
 SpawnUnitBehaviorModuleData::~SpawnUnitBehaviorModuleData() { m_famgen = 0; }
 void famgenDeleteSpawnUnitBehaviorModuleData(SpawnUnitBehaviorModuleData *p) { delete p; }
+
+// ??_GCommandSetUpgradeModuleData@@UAEPAXI@Z @0x00256267 28B
+// Deleting dtor slot 0 of vtable 0x007F37C0 (shared ICF with AttributeModifier 0x7F38C0 Castle 0x7F3940);
+// calls rowed ??1 at 0x002561EF then rowed operator delete at 0x0002FD60.
+class CommandSetUpgradeModuleData { public: __declspec(noinline) virtual ~CommandSetUpgradeModuleData(); private: int m_famgen; };
+// ??1CommandSetUpgradeModuleData@@UAE@XZ present-unmatched
+CommandSetUpgradeModuleData::~CommandSetUpgradeModuleData() { m_famgen = 0; }
+void famgenDeleteCommandSetUpgradeModuleData(CommandSetUpgradeModuleData *p) { delete p; }
