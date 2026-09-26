9de9b2e651fe0fe6dba5d4b8a2babb96918b2aec
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 17:05:34 +0000

    Recover GameWindowManager::winPrevTab 123B @0x002C5473 via BFME1 donor verbatim

---
+++ Code/GameEngine/Source/GameClient/GUI/GameWindowManager_winPrevTab.cpp
@@ -0,0 +1,61 @@
+// cl: /O1 /DNDEBUG /MD /EHsc
+// stlport
+// ?winPrevTab@GameWindowManager@@UAEXPAVGameWindow@@@Z @0x002C5473 123B
+// GameWindowManager::winPrevTab; BFME1 donor GameWindowManager.cpp winPrevTab verbatim; m_tabList at +0x30 m_modalHead at +0x24; vtable 0x7C7C90 slot 43 offset 0xAC; calls winSetFocus slot 49 0xC4 and winSetLoneWindow slot 52 0xD0.
+#include <list>
+
+class GameWindow;
+
+typedef _STL::list<GameWindow *> GameWindowList;
+
+class GameWindowManager
+{
+public:
+#define V(n) virtual void pad##n() = 0;
+	V(0) V(1) V(2) V(3) V(4) V(5) V(6) V(7)
+	V(8) V(9) V(10) V(11) V(12) V(13) V(14) V(15)
+	V(16) V(17) V(18) V(19) V(20) V(21) V(22) V(23)
+	V(24) V(25) V(26) V(27) V(28) V(29) V(30) V(31)
+	V(32) V(33) V(34) V(35) V(36) V(37) V(38) V(39)
+	V(40) V(41) V(42)
+#undef V
+	virtual void winPrevTab(GameWindow *window);
+#define W(n) virtual void pad##n() = 0;
+	W(44) W(45) W(46) W(47) W(48)
+#undef W
+	virtual int winSetFocus(GameWindow *window);
+#define X(n) virtual void pad##n() = 0;
+	X(50) X(51)
+#undef X
+	virtual void winSetLoneWindow(GameWindow *window);
+
+private:
+	unsigned char m_pad04[0x20];
+	void *m_modalHead;
+	unsigned char m_pad28[0x08];
+	GameWindowList m_tabList;
+};
+
+void GameWindowManager::winPrevTab(GameWindow *window)
+{
+	if (m_tabList.size() == 0 || m_modalHead)
+		return;
+
+	GameWindowList::reverse_iterator it = m_tabList.rbegin();
+	while (it != m_tabList.rend())
+	{
+		if (*it == window)
+		{
+			it++;
+			break;
+		}
+		it++;
+	}
+	if (it != m_tabList.rend())
+		winSetFocus(*it);
+	else
+	{
+		winSetFocus(*m_tabList.rbegin());
+	}
+	winSetLoneWindow(0);
+}
