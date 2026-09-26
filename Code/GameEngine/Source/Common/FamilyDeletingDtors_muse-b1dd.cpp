a97e07081237bbebd2532e0c463bc0ac33ca8957
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-26 14:45:39 +0000

    Recover ReplaceSelfUpgrade deleting dtor 28B @0x004B70CF via family O1

--- Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-b1dd.cpp
+++ Code/GameEngine/Source/Common/FamilyDeletingDtors_muse-b1dd.cpp
@@ -4,3 +4,10 @@
 class ReplaceSelfUpgradeModuleData { public: __declspec(noinline) virtual ~ReplaceSelfUpgradeModuleData(); private: int m_famgen; };
 ReplaceSelfUpgradeModuleData::~ReplaceSelfUpgradeModuleData() { m_famgen = 0; }
 void famgenDelete(ReplaceSelfUpgradeModuleData *p) { delete p; }
+
+// ??_GReplaceSelfUpgrade@@MAEPAXI@Z @0x004B70CF 28B
+// Deleting dtor slot 0 of vtable 0x00858B10; calls rowed ??1ReplaceSelfUpgrade@@MAE@XZ at 0x004B70EB then rowed operator delete at 0x0002FD60.
+class ReplaceSelfUpgrade { protected: __declspec(noinline) virtual ~ReplaceSelfUpgrade(); private: int m_famgen;
+  friend void famgenDelete(ReplaceSelfUpgrade *p); };
+ReplaceSelfUpgrade::~ReplaceSelfUpgrade() { m_famgen = 0; }
+void famgenDelete(ReplaceSelfUpgrade *p) { delete p; }
